72433d6e8313e3c20ae7d932a9988559d7014318
Riwandy <riwandys@gmail.com>
2019-11-20 18:05:31 +0700

    Menambahkan kerangka header dan footer

---
+++ application/views/template/footer.php
@@ -0,0 +1,9 @@
+    <footer>
+        <p>Dikembangkan oleh: <br></p>
+        <ol>
+            <li>Riwandy (14117098)</li>
+            <li>Jefri Manurung (14117107)</li>
+        </ol>
+    </footer>
+</body>
+</html>
