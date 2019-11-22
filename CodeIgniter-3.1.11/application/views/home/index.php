c083d6ba96cdb02fe4bc80523e1428faf788175d
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2019-11-23 01:51:32 +0700

    Memodifikasi dan Merapihkan Tampilan Home

--- CodeIgniter-3.1.11/application/views/home/index.php
+++ CodeIgniter-3.1.11/application/views/home/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SIMPLE LAPOR!!</title>
+	<title>SIMPLE LAPOR!</title>
 	<!-- file css ada di asset/css -->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/asset/css/style.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/asset/css/reset.css">
@@ -9,28 +9,38 @@
 <body>
 
 <div class="container">
-	<h1>SIMPLE LAPOR!</h1>
+	<h1>SIMPLE LAPOR !</h1>
 	<form action="" method="post">
-		<input type="text" name="cari">
-		<button type="submit" name="submit">CARI</button>
+		<input type="text" name="cari"> </input>
+		<button type="submit" name="submit"> CARI </button>
 	</form>
 
-	<a href="" ><h3>buat laporan / komentar</h3></a>
-	<h4>laporan / komentar terakhir</h4>
+	<a href="" ><h3>Buat Laporan / Komentar</h3></a>
+	<h4>Laporan / Komentar Terakhir</h4>
 	<hr />
+	
 	<ul>
 	<?php foreach ($lapor as $value){ ?> <!--$lapor didapet dari controller home -->
 		<li>
 			<h4 style="margin-bottom: 20px;"><?php echo $value["isi"]; ?></h4>
-			<h4 class="lampiran">lampiran : foto.jpg</h4>
-			<a href=""><h4 class="detail">selengkapnya>></h4></a>
-			<h4 class="waktu" style="margin-right: 10px;">waktu : <?php echo $value["tanggal"]; ?></h4>
+			<h4 class="lampiran">Lampiran : foto.jpg</h4>
+			<a href=""><h4 class="detail">Selengkapnya >>></h4></a>
+			<h4 class="waktu" style="margin-right: 10px;">Waktu : <?php echo $value["tanggal"]; ?></h4>
 			<div style="clear: both"></div>
 		</li>
+		<br>
 		<hr />
 	<?php } ?>
 	</ul>
+
 </div>
+
+</div>
+
+<div id="footer">
+&copyCopyright 2019 - itera.ac.id
+</div>
+
 
 </body>
 </html>
