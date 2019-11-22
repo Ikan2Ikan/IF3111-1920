6efa47ada2036e8df2aad77b55da2ee9a317444f
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-11-22 14:19:39 +0700

    Menambahkan controller dan view login

---
+++ lapor-app/application/views/login/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login page</title>
+</head>
+
+<body>
+  <label for="username">username</label>
+  <input type="text" name="username" id="username" placeholder="username">
+  <br>
+  <label for="password">password</label>
+  <input type="password" name="password" id="password" placeholder="password">
+  <br>
+  <button type="submit">Login</button>
+</body>
+
+</html>
