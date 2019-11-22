a0f0ddcaefb87dcf7c5a5b57b434b2a9f48d0b8d
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-11-22 21:08:44 +0700

    membuat controler dan view register

---
+++ lapor-app/application/views/register/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Register page</title>
+</head>
+
+<body>
+  <label for="username">username</label>
+  <input type="text" name="username" id="username" placeholder="username">
+  <br>
+  <label for="email">email</label>
+  <input type="text" name="email" id="email" placeholder="email">
+  <br>
+  <label for="password">password</label>
+  <input type="password" name="password" id="password" placeholder="password">
+  <br>
+  <label for="password2">ulangi password</label>
+  <input type="password" name="password2" id="password2" placeholder="ulangi password">
+  <br>
+  <button type="submit">Login</button>
+</body>
+
+</html>
