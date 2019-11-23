97a85feed17bfa7a8b57b9ee81b7243d5eb88f3e
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-11-23 22:17:07 +0700

    menambahkan halaman utama
    halaman utama simple lapor app

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,5 +1,26 @@
+<!doctype html> 
+ 
+<html lang="en">
+   <head> 
+      <meta charset="utf-8">
+      <title>Lapor App</title>
+      
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+   </head>
+<body> 
+
+<h1>SIMPLE LAPOR!</h3>
+
+<form>
+  <input class="search" type="text" placeholder="Cari..." required>	
+  <input class="button" type="button" value="Cari">		
+</form>
+    <a href="">Buat Laporan / Komentar </a>
+</body>
+</html>
 
 
 
-<a href="">Buat Laporan / Komentar </a>
+
+
 
