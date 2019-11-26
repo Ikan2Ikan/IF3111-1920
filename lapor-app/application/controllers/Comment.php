a795af2defc4c54a3fa9a56416fda3dd8d8b53a9
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-11-26 22:13:44 +0700

    Menambahkan file header untuk comment agar styling css tidak bertabrakan

--- lapor-app/application/controllers/Comment.php
+++ lapor-app/application/controllers/Comment.php
@@ -3,9 +3,9 @@ class Comment extends CI_Controller
 {
     public function index()
     {
-        $data = "Tambah Komentar/Laporan";
-        $this->load->view('templates/header', $data);
+        $data['title'] = "Tambah Komentar/Laporan";
+        $this->load->view('templates/comm_header', $data);
         $this->load->view('comment/index.php', $data);
-        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/footer');
     }
 }
