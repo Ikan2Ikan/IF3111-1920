10c36293a7be4ee537d6ab99f157bc7c629541e1
Jefri Manurung <jefrymanurung23@gmail.com>
2019-11-27 00:16:17 +0700

    menambahkan placeholder pada search-bar

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,7 +2,7 @@
     
 </div>
 <div class="searchbar">
-    <input type="text" name="search" id="search-bar">
+    <input type="text" name="search" id="search-bar" placeholder="Kata kunci atau tracking ID...">
     <input type="button" value="Cari" id="search-button">
 </div>
 <div class="lapor">
