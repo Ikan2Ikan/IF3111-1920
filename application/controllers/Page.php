89eac74782dc7ce99c487cce2d9ca387f073f918
Riwandy <riwandys@gmail.com>
2019-11-26 17:19:11 +0700

    Penambahan folder css

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Page extends CI_Controller {
-	public function index($halaman = 'home')
-	{
-        $this->load->view('template/header');
-        $this->load->view('pages/'.$halaman);
-        $this->load->view('template/footer');
+	public function index($halaman = 'home'){
+                $this->load->helper('url');
+                $this->load->view('template/header');
+                $this->load->view('pages/'.$halaman);
+                $this->load->view('template/footer');
 	}
 }
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,8 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LAPOR!</title>
+    <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css">
 </head>
 <body>
     <header>
-        <h1>Simple LAPOR!</h1>
+        <h1>SIMPLE LAPOR!</h1>
     </header>
