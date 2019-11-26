daefaa92b2dd5bf48fc4a7ed4ca55913afc51b9f
Jefri Manurung <jefrymanurung23@gmail.com>
2019-11-27 00:14:18 +0700

    menambahkan google font open sans

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LAPOR!</title>
     <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+
 </head>
 <body>
     <header>
