a2747a0c7f973db8eeea7b078f489922cfb44de7
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-11-26 15:55:14 +0700

    membuat view templates, form login, dan register

--- lapor-app/application/controllers/Auth.php
+++ lapor-app/application/controllers/Auth.php
@@ -1,15 +1,38 @@
 <?php
 class Auth extends CI_Controller
 {
-  public function index($nama = "")
+  public function __construct()
   {
-    $data['nama'] = $nama;
-    $this->load->view('auth/login', $data);
+    parent::__construct();
+    $this->load->model('User_model');
+    // $this->load->library('database');
   }
+
+  public function index()
+  {
+    $data['title'] = "User Login";
+    // $data['users'] = $this->User_model->getAllUser();
+    $this->load->view('templates/header', $data);
+    $this->load->view('auth/login', $data);
+    $this->load->view('templates/footer', $data);
+    // $username = $this->input->post('username');
+    // $password = $this->input->post('password');
+    // $result = $this->db->get_where('user', ['username' => $username])->result_array();
+    // var_dump($result);
+  }
+
   public function register()
   {
     $data['title'] = "Register page";
     $data['username'] = $this->input->post('username');
+    $this->load->view('templates/header', $data);
     $this->load->view('auth/register', $data);
+    $this->load->view('templates/footer', $data);
+  }
+
+  public function insert()
+  {
+    $this->User_model->insertUserData();
+    redirect('user');
   }
 }
--- lapor-app/application/views/auth/register.php
+++ lapor-app/application/views/auth/register.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?= $title; ?></title>
-</head>
-
-<body>
   <form action="<?= base_url() ?>user/insert" method="post">
     <label for="username">username</label>
     <input type="text" name="username" id="username" placeholder="username">
@@ -24,6 +13,4 @@
     <br>
     <button type="submit">Login</button>
   </form>
-</body>
-
-</html>
+  <a class="small" href="<?= base_url('auth') ?>">Already have an account? Login!</a>
