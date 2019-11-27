e33da0af8338645097ff2da9c622e00a87e58928
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-11-28 06:01:58 +0700

    menambahkan model detail
    model detail for detail laporan

--- application/models/Detail_model.php
+++ application/models/Detail_model.php
@@ -3,7 +3,10 @@ class Detail_model extends CI_model{
     public function getDetailLaporan(){
         // return $this->db->get('simple_lapor')->result_array();
         $id = $_GET["detail_id"];
-		return "SELECT * FROM simple_lapor WHERE id_lapor= '$id'";
+        // return "SELECT * FROM simple_lapor WHERE id_lapor= '$id'";
+        // return $this->db->get('simple_lapor')->result_array();
+        $query = $this->db->get_where('simple_lapor', array('id_lapor' => $id));
+        return $query->result_array();
     }
    
 }
