7b58bdebd70752cf9d7571866657354fbc7a13ad
aldiindrawan <45248095+aldiindrawan@users.noreply.github.com>
2019-11-27 20:28:49 +0700

    fix view detail dan tambah laporan

--- CodeIgniter-3.1.11/application/controllers/Tambah.php
+++ CodeIgniter-3.1.11/application/controllers/Tambah.php
@@ -5,7 +5,6 @@ class Tambah extends CI_Controller
 {
 	
 	public function index(){
-		$data['nama'] = "aldi indrawan";
-		$this->load->view('tambah/index',$data);
+		$this->load->view('tambah/index');
 	}
 }
--- CodeIgniter-3.1.11/application/views/tambah/index.php
+++ CodeIgniter-3.1.11/application/views/tambah/index.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>SIMPLE LAPOR!</title>
+	<!-- file css ada di asset/css -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/asset/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/asset/css/reset.css">
 </head>
 <body>
-<h1>hallo, <?php echo $nama ?></h1>
+
+<div class="container">
+	<h1>SIMPLE LAPOR !</h1>
+    <h4>Buat Laporan/Komentar</h4>
+   <hr />
+
+   <form method="post" action="" enctype="">
+	    <textarea rows="16" cols="136" name="isi" form="usrform"></textarea>
+		<select name="aspek">
+			<option>- Pilih Aspek Pelaporan/Komentar -</option>
+			<option value="Dosen">Dosen</option>
+			<option value="Staff">Staff</option>
+			<option value="Mahasiswa">Mahasiswa</option>
+			<option value="Infrastruktur">Infrastruktur</option>
+			<option value="Pengajaran">Pengajaran</option>
+		</select><br />
+		<input id="file" type="file" name="File"><br />
+		<input id="submit" value="Tambah" type="submit" name="submit">
+		<div style="clear: both;"></div>
+	</form>
+	<hr>
+	<footer>
+		&copyCopyright 2019 - itera.ac.id
+	</footer>
+
+</div>
 </body>
 </html>
