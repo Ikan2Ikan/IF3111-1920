1ecc44dc95f6e0e100474ea641703df471f2d6a2
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-11-28 13:08:19 +0700

    Update controller Buat.php

--- application/controllers/Buat.php
+++ application/controllers/Buat.php
@@ -16,7 +16,7 @@ class Buat extends CI_Controller{
        
         $this->form_validation->set_rules('laporan','Laporan','required');
         $this->form_validation->set_rules('aspek','Aspek','required');
-        $this->form_validation->set_rules('myFile','File','required');
+        // $this->form_validation->set_rules('myFile','File','required');
         
         if($this->form_validation->run() == FALSE){
             $data ['judul'] = 'Buat Laporan';
@@ -27,35 +27,6 @@ class Buat extends CI_Controller{
             redirect('home');
         }
     }
-
-    public function do_upload(){
-        $config['upload_path']          = './gambar/';
-        $config['allowed_types']        = 'gif|jpg|png';
-        $config['max_size']             = 100;
-        $config['max_width']            = 1024;
-        $config['max_height']           = 768;
-     
-        $this->load->library('upload', $config);
-     
-        if ( ! $this->upload->do_upload('myFile')){
-            $error = array('error' => $this->upload->display_errors());
-            $this->load->view('buat_laporan/index', $error);
-        }else{
-            $data = array('upload_data' => $this->upload->data());
-            $data = [ 
-                "laporan" => $this->input->post('laporan',true),
-                "lampiran" => $this->input->data(),
-                "waktu" => $this->input->post('$tgl',true),
-                "aspek" => $this->input->post('aspek',true)
-            ];
-
-            // $this->load->view('v_upload_sukses', $data);
-            $this->db->insert('simple_lapor', $data);
-        }
-       
-        }
-        
-
 
 
 }
