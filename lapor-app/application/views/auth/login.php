f587ef4c5385a4c66eec5a27ed7b9bb7b5aa41c3
Dhiko <your@email.com>
2019-11-28 08:23:25 +0700

    css login

--- lapor-app/application/views/auth/login.php
+++ lapor-app/application/views/auth/login.php
@@ -1,10 +1,18 @@
-  <form action="<?= base_url(); ?>auth" method="post">
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" placeholder="email">
-    <br>
-    <label for="password">password</label>
-    <input type="password" name="password" id="password" placeholder="password">
-    <br>
-    <button type="submit" name="login">Login</button>
-  </form>
-  <a href="<?= base_url('auth/register');  ?>">Create an Account!</a>
+  <div class="container">
+    <form action="<?= base_url(); ?>auth" method="post">
+      <label for="email">Email : </label>
+      <br>
+      <input type="text" name="email" id="email" placeholder="Email">
+      <br>
+
+      <label for="password">Password : </label>
+      <br>
+      <input type="password" name="password" id="password" placeholder="Password">
+      <br>
+      <br>
+
+      <button type="submit" name="login">Login</button>
+    </form>
+
+    <a href="<?= base_url('auth/register');  ?>">Create an Account!</a>
+  </div>
