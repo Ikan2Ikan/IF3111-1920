fcf04531d5bc4f5643d9279093bc9926d8216a04
Jefri Manurung <jefrymanurung23@gmail.com>
2019-11-28 23:27:44 +0700

    menambahkan dropdown(select) pada buat laporan

--- application/views/pages/add.php
+++ application/views/pages/add.php
@@ -3,7 +3,7 @@
 		<!-- <p>Buat Laporan/Komentar</p>
 		<hr> -->
 		<textarea placeholder="Ketik Laporan Anda..."></textarea>
-		<!-- <div class="custom-select">
+		<div class="custom-select">
 			<select name="">
 				<option hidden>Pilih Aspek Pelaporan/Komentar</option>
 				<option value="dosen">Dosen</option>
@@ -12,6 +12,6 @@
 				<option value="infrastruktur">Infrastruktur</option>
 				<option value="pengajaran">Pengajaran</option>
 			</select>
-		</div> -->
+		</div>
 	</div>
 </div>
