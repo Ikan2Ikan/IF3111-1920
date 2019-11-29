cf112ef83b1c9205a8d4c49c53e16e599bbbf839
Deniesh Nathanael Virginiel <denieshnv@gmail.com>
2019-11-29 09:30:39 +0700

    Pembuatan Class Container

--- application/views/konten/template.php
+++ application/views/konten/template.php
@@ -1,12 +1,15 @@
 
-    <div class="search">
-        <input type="text" name="searchbox" id="search-box" placeholder="Telusuri laporan">
-        <input type="button"name="searchbutton" id = "search-button" value="Cari">
-    </div>
-    <div class="buat">
-    <a href="index.php/buatlaporan">
-        Buat Laporan/Komentar
-    </a>
+    <div class="container">
+
+        <div class="search">
+            <input type="text" name="searchbox" id="search-box" placeholder="Telusuri laporan">
+            <input type="button"name="searchbutton" id = "search-button" value="Cari">
+        </div>
+        <div class="buat">
+        <a href="index.php/buatlaporan">
+            Buat Laporan/Komentar
+        </a>
+        </div>
     </div>
     
 </body>
