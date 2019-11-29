6adca9c9da0ef8369230c711e1db8f544ea229b6
dannyambarita <48155543+dannyambarita@users.noreply.github.com>
2019-11-29 12:41:26 +0700

    membuat kolom laporan terakhir
    membuat kolom laporan terakhir

--- application/views/konten/template.php
+++ application/views/konten/template.php
@@ -11,7 +11,17 @@
         </a>
         </div>
         <div class="laporan_terakhir">
-            =lorem()
+        Laporan/Komentar Terakhir<br>
+        <hr>
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut quasi voluptatibus magnam beatae nisi ea, nulla autem ut ipsa odit tempora saepe error culpa ratione asperiores, accusamus harum eum.<br>
+        Lampiran: Gambar.jpg <br>
+        <hr>
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, ipsam consectetur vel ea quod, odio dolorum atque qui officia, laborum numquam laudantium libero exercitationem ad. Nisi illo assumenda quasi hic! <br>
+        Lampiran: Gambar.doc<br>
+        <hr>
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet ut at, magnam aliquam earum repellat, pariatur praesentium soluta explicabo temporibus iste. Mollitia eveniet id molestias ipsam vel quis sapiente. Eius. <br>
+        Lampiran: Gambar.jpg <br>
+        <hr>
         </div>
     </div>
     
