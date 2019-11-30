b2fdcc6cc1b9e5f9e8d435fca57ca5d29b9fba27
Achmad Bany Majesty <50118567+BanyVengeanceA7X@users.noreply.github.com>
2019-11-30 20:03:27 +0700

    menambahkan beranda.php
    file ini merupakan tampilan pertama ketika membuka website

---
+++ TUBES/application/views/beranda.php
@@ -0,0 +1,20 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE HTML>
+<HTML lang="en-US">
+    <body>
+
+        <div class="SearchBar">
+            <form>
+                <input type="text" placeholder="" name="search ">
+            </form>
+        </div>
+
+        <div class="buat">
+        <a href="<?= base_url(); ?>createform">Buat Laporan/Komentar </a>
+        </div>
+        
+    </body>
+</HTML>
