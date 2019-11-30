dd9bc68a77c3c9f97326064caa54e53226bb7c58
Jefri Manurung <jefrymanurung23@gmail.com>
2019-11-30 21:49:00 +0700

    mengubah tampilan tombol upload file

--- js/script.js
+++ js/script.js
@@ -87,4 +87,24 @@ window.onload = function () {
 	// 	this.style.height = (this.scrollHeight) + 'px';
 	// }
 	/*end textarea*/
+
+	/*start file upload*/
+	const realFileBtn = document.getElementById("real-file");
+	const customBtn = document.getElementById("custom-button");
+	const customTxt = document.getElementById("custom-text");
+
+	customBtn.addEventListener("click", function () {
+		realFileBtn.click();
+	});
+
+	realFileBtn.addEventListener("change", function () {
+		if (realFileBtn.value) {
+			customTxt.innerHTML = realFileBtn.value.match(
+				/[\/\\]([\w\d\s\.\-\(\)]+)$/
+			)[1];
+		} else {
+			customTxt.innerHTML = "No file chosen, yet.";
+		}
+	});
+	/*end file upload*/
 }
