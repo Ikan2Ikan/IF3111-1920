f95b79a876f4a160cf1c629dda3d816946162674
Achmad Bany Majesty <50118567+BanyVengeanceA7X@users.noreply.github.com>
2019-11-30 20:01:57 +0700

    menambahkan file desain.php
    file ini merupakan kumpulan dari css yang akan digunakan pada pemanggilan file-file yang membutuhkannya

---
+++ TUBES/application/views/desain.php
@@ -0,0 +1,96 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE HTML>
+<HTML>
+
+<head>
+    <title>FORM LAPOR</title>
+    <style type="text/css">
+        body {
+            font-family: 🌉 <!--font sansfrancisco-->
+        }
+        
+        a {
+            text-decoration: none;
+            color: black;
+            <!--untuk tulisan link buat lapor/komentar-->
+        }
+        
+        .judul {
+            font-size: 50px;
+            outline-style: none;
+        }
+        
+        .inputjudul {
+            margin-left: 225px;
+            border: white;
+            font-size: 15px;
+        }
+        
+        .SearchBar {
+            position: absolute;
+            top: 300px;
+            left: 520px;
+        }
+        
+        .SearchBar input {
+            position: center;
+            width: 900px;
+            height: 50px;
+            font-size: 50px;
+        }
+        
+        .buat {
+            position: absolute;
+            top: 390px;
+            left: 870px;
+            color: black;
+            text-decoration: none;
+        }
+        
+        .loadkomentar {
+            border: none;
+        }
+        
+        .opsi {
+            font-size: 15px;
+            margin-left: 223px;
+            height: 35px;
+            width: 240px;
+        }
+        
+        .komentar {
+            margin-top: 20px;
+            margin-left: 223px;
+            font-size: 15px;
+            name="komentar";
+            height: 300px;
+            width: 1455px;
+        }
+        
+        .lampiran {
+            margin-left: 223px;
+            color: #C0C0C0;
+            font-size: 15px;
+            width: 200px;
+        }
+        
+        .submit {
+            margin-left: 1580px;
+            margin-top: 20px;
+            height: 55px;
+            font-size: 15px;
+        }
+    </style>
+
+<body>
+    <center>
+        <br><br>
+        <p class="judul"> SIMPLE LAPOR!</h1>
+        </p>
+    </center>
+</body>
+
+</head>
