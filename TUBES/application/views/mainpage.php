1ac17a3dd1f915961864923965ae4ace7daf207f
Yose Saragih <yosesrgh@gmail.com>
2019-11-30 22:36:06 +0700

    ADD mainpage

---
+++ TUBES/application/views/mainpage.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Main Page</title>
+        <link rel="stylesheet" href="asset/mainpage.css"/>
+    </head>
+    <table border="0" align="center" width="1000px">
+        <form>
+        <tr>
+            <center>
+                <h1>SIMPLE LAPOR!</h1>
+            </center>
+        </tr>
+        <tr>
+            <td>
+                <input class="search" [type="text"]/> <input class="button1" type="submit" value="Cari"/>
+                <center>
+                    <p>Buat Laporan/Komentar
+                        <button class="button2">`</button>
+                    </p>
+                </center>
+            </td>
+        </tr>
+<!-- Temporary
+        <tr>
+            <td>
+                <p> </p>
+                <p>Laporan/Komentar terakhir</p>
+                <hr>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea 
+                commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                non proident, sunt in culpa qui officia deserunt mollit 
+                anim id est laborum.(80 words)
+                </p>
+                <br>
+                Lampiran : 
+            </td>
+        </tr>
+        <tr>
+            <td>
+            -->
+        </form>
+</html>
