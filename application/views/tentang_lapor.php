7c0202a1cc8f4b1f22314a96e6fc521d04a56f07
Muhammad Rizki Oktarlis Setia Budi <mrizki.14115032@student.itera.ac.id>
2019-12-01 08:50:13 +0700

    template lapor

---
+++ application/views/tentang_lapor.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+    <!-- <link rel="stylesheet" href=" /assets/font/font-awesome.min.css"> -->
+    <title>Tentang Lapor</title>
+</head>
+
+<body>
+    <section class="intro">
+        <div class="inner">
+            <div class="content">
+                <h1>LAPORKAN MASALAHMU!!</h1>
+                <a href="#" class="btn">Lanjutkan</a>
+            </div>
+        </div>
+    </section>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum repellat quos et, enim totam corrupti fugit voluptates ut porro. Ab veritatis illo harum alias ad corporis rerum aspernatur labore beatae!</p>
+</body>
+
+</html>
