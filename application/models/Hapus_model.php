b197be433f13d01153f01d011ab3d0345e1e8c1e
Alvijar <alvijarp11@gmail.com>
2019-12-02 14:21:07 +0700

    update hapus_model.php
    menambahkan javascript menghapus data

--- application/models/Hapus_model.php
+++ application/models/Hapus_model.php
@@ -1,11 +1,15 @@
 <?php
 class Hapus_model extends CI_model{
     public function getHapusLaporan(){
-        // return $this->db->get('simple_lapor')->result_array();
+
         $id = $_GET["hapus_id"];
-        // return "SELECT * FROM simple_lapor WHERE id_lapor= '$id'";
-        // return $this->db->get('simple_lapor')->result_array();
+       
         $this->db->delete('simple_lapor', array('id_lapor' => $id));
+        echo "<script>alert('data terhapus');</script>";
+        echo "<script>location='home';</script>"; 
+     
+     
+
        
     }
    
