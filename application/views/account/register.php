0bb14c19982c1e35fe4e7827ce0c5a51e404cf08
majidilkhithar <nuralimajids@gmail.com>
2019-12-02 16:33:10 +0700

    upload tampilan daftar dan css

---
+++ application/views/account/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar-LAPOR!</title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/account.css" type="text/css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+</head>
+
+<body>
+    <div class="row-flex">
+        <div class="bag-kiri">
+            <div class="col-sm-6">
+                <div class="header">
+                    <h1 class="text-center">Daftar</h1>
+                </div>
+
+            </div>
+        </div>
+        <div class="bag-kanan"></div>
+    </div>
+</body>
+
+</html>
