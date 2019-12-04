d6cafd2c095ce7fe9c69b61da3272b43a5781d57
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-12-04 12:10:29 +0700

    membuat model pencarian

---
+++ application/models/Cari_model.php
@@ -0,0 +1,21 @@
+<?php
+class Home_model extends CI_model{
+    public function getAllHome(){
+        // return $this->db->get('simple_lapor',)->result_array();
+        
+        // $this->db->order_by("id_lapor", "desc");
+        return $this->db->get('simple_lapor')->result_array();
+        // $query = $this->db->get();
+        // return $query->result();
+    }
+    public function get_keyword($keyword){
+        $this->db->select('*');
+        $this->db->from('simple_lapor');
+        $this->db->like('laporan', $keyword);
+        $this->db->or_like('aspek', $keyword);
+        // $this->db->or_like('lampiran', $keyword);
+        return $this->db->get()->result();
+    }
+}
+
+?>
