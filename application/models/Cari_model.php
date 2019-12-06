a3441d0c84a3244937251d9d9d7c83d9ef447e04
Alvijar <alvijarp11@gmail.com>
2019-12-06 17:04:25 +0700

    hapus beberapa fungsi tidak terpakai

--- application/models/Cari_model.php
+++ application/models/Cari_model.php
@@ -1,21 +1,26 @@
 <?php
 class Cari_model extends CI_model{
-    public function getAllHome(){
-        // return $this->db->get('simple_lapor',)->result_array();
-        
-        // $this->db->order_by("id_lapor", "desc");
-        return $this->db->get('simple_lapor')->result_array();
-        // $query = $this->db->get();
-        // return $query->result();
-    }
-    public function get_keyword($keyword){
-        $this->db->select('*');
-        $this->db->from('simple_lapor');
-        $this->db->like('laporan', $keyword);
-        $this->db->or_like('aspek', $keyword);
-        // $this->db->or_like('lampiran', $keyword);
-        return $this->db->get()->result();
-    }
+
+    public function cariData($keyword = null)
+	{
+		if($keyword){
+			$this->db->like('laporan', $keyword);
+			$this->db->or_like('aspek',$keyword);
+		}
+		return $this->db->get('simple_lapor')->result_array();
+		// $cari = $this->input->GET('cari', TRUE);
+		// $data = $this->db->query("SELECT * from simple_lapor where laporan like '%$cari%' ");
+		// return $data->result_array();
+
+			// $this->db->select('*');
+			// $this->db->from('simple_lapor');
+			// $this->db->like('laporan',$keyword);
+			// $this->db->or_like('aspek',$keyword);
+			// return $this->db->get()->result();
+	}
+
+
+
 }
 
 ?>
