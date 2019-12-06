527277674d72394dcb1d1acb3619f4456c68d33b
wahyuwiranti <wahyuwiranti@gmail.com>
2019-12-06 20:21:22 +0700

    TAMBAH Judul dan Isi

---
+++ application/views/v_detail.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+	<div id="body">
+	<h1><center>SIMPLE LAPOR!<center></h1>
+	<p>Detail Laporan/Komentar</p>
+	<p>Lorens ipsum dolor sit amet, consectetur adipiscing eliT. Nunc maximus, nulla ut commado sapittis, sapien dui mattis dui, non pulvinar lorem felis nec erat. torem iptum dolor sit amet, consecterur adipiscing elit. Nunc maximus, rulta ut comnodo sagittin, sapien dui mattis dui, non pulvinar lorem felis nec erat. Lorem psum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, saplen dui mattis pulvinar lorem felis nec erat</p>
+	<p>Lorem ipsum dolor sit amet, torisectetur adipiscing elit. Nunc maimus, inula ut commodo sagittis, sapier dui mattis dui, non pulvinar mattis dui, non pulvinar larem felis nec erat. Lorem psum dolor sit amat, comactetur adipiscing elit. Nunc maximus, nulla ut commodo Jorem tei sugittis, sapien dui mattis dui, non pulvinar lorem felis net erat nec erat. L Lorem ipsu im dolor sit amet, consectetur adipiscing elit. Nunc maximus, rulla ut commodo sagittis, sapien dui, non pulvinar lorem felia nec erat</p>
+	
+	<p class="footer">
+		<div class="container my-auto">
+		<div class="waktu text-left my-auto">
+		<span>Waktu : <?php echo Date('D-M-Y') ?> &nbsp; Aspek: Infrastruktur Hapus Laporan/Komentar</span>
+		</div>
+		</div>
+	</footer>
+	</div>
+</body>
+</head>
+</html>
