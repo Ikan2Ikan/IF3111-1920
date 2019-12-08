527c354c26da7ad7fa58f401b0bb06dee051d220
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2019-12-08 18:25:51 +0700

    Finish Header Footer
    menyelesaikan header dan footer yang tadinya belum selesai

--- Lapor/application/views/templates/footer.php
+++ Lapor/application/views/templates/footer.php
@@ -1,2 +1,5 @@
+	<div class="footer">
+  		<p>Created by Laurensius Joshua Anrico Agustinus and Krisdiyansyah</p>
+	</div>
 </body>
 </html>
--- Lapor/application/views/templates/header.php
+++ Lapor/application/views/templates/header.php
@@ -4,5 +4,13 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	<h3>home</h3>
+	<div class="topnav">
+		<div class="logo">
+  			<img src="assets/img/logolapor.png" alt="logo_lapor" width="70"/>
+  		</div>
+		<a href="home">Home</a>
+	  	<a href="about">About</a>
+	  	<a style="float:right" href="signin">Sign - In</a>
+	  	<a style="float:right" href="singup">Sign - Up</a>
+	</div>
 	
