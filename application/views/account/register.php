89706dc3af09359c48827319b991c89c92ff82cc
majidilkhithar <nuralimajids@gmail.com>
2019-12-09 18:38:20 +0700

    Update register.php

--- application/views/account/register.php
+++ application/views/account/register.php
@@ -14,8 +14,10 @@
     <div class="row-flex">
         <div class="bag-kiri">
             <div class="col-sm-6">
-                <div class="header">
-                    <h1 class="text-center">Daftar</h1>
+                <div class="col-sm-10 col-sm-push-1 col-8">
+                    <div class="header">
+                        <h1 class="text-center">Daftar</h1>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-10 col-sm-push-1 col-8">
@@ -23,11 +25,14 @@
                             <span>Gunakan Akun Media Sosial</span>
                         </h5>
                         <div class="row">
-                            <div class="col-6">
-                                <img src="<?= base_url() ?>assets/img/facebook.png" alt="facebook">
-                                <img src="<?= base_url() ?>assets/img/twitter.png" alt="twitter">
+                            <div class="col-sm-10 col-sm-push-1 col-8">
+                                <div class="col-6">
+                                    <img src="<?= base_url() ?>assets/img/facebook.png" alt="facebook">
+                                </div>
+                                <div class="col-6">
+                                    <img src="<?= base_url() ?>assets/img/twitter.png" alt="twitter">
+                                </div>
                             </div>
-
                         </div>
                     </div>
 
