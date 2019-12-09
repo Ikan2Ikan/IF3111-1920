29838b3f96d3109dfa98ba3fafc7d7e5d53162e3
Jefri Manurung <jefrymanurung23@gmail.com>
2019-12-09 17:07:59 +0700

    Membuat variabel $data['posts']

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -3,16 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Page extends CI_Controller {
 	public function index($halaman = 'home'){
-                $this->load->helper('url');
-                $this->load->view('template/header');
-                $this->load->view('pages/'.$halaman);
-                $this->load->view('template/footer');
+        $data['posts'] = $this->post_model->get_posts();
+
+        $this->load->helper('url');
+        $this->load->view('template/header');
+        $this->load->view('pages/'.$halaman, $data);
+        $this->load->view('template/footer');
     }
     
     public function add($halaman = 'add'){
-                $this->load->helper('url');
-                $this->load->view('template/header');
-                $this->load->view('pages/'.$halaman);
-                $this->load->view('template/footer');
+        $this->load->helper('url');
+        $this->load->view('template/header');
+        $this->load->view('pages/'.$halaman);
+        $this->load->view('template/footer');
     }
 }
