cab075a781b325f4c1c242a194b44e8fff9f6799
Nuranisda Triawati / 14117043 <50206305+nuranisda@users.noreply.github.com>
2019-12-09 11:19:40 +0700

    menambahkan judul, search bar, dan css

---
+++ Tubes-CI/application/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Halaman utama</title>
+        <link rel="stylesheet" href="aset/style.css">
+    </head>
+
+    <body>
+        <h1>SIMPLE LAPOR</h1>
+        
+        <form>
+            <input class="search" type="text" placeholder="" required>	
+            <input class="button" type="button" value="Cari">		
+        </form>
+        
+        <p>Buat Laporan/Komentar</p>
+        
+
+    </body>
+
+</html>
