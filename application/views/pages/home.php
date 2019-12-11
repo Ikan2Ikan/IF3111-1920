28a375b56d4913aeb26eb92248b255fb760cc973
Riwandy <riwandys@gmail.com>
2019-12-11 21:58:56 +0700

    Penambahan file .htaccess

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,7 +3,7 @@
     <input type="button" value="Cari" id="search-button">
 </div>
 <div class="lapor">
-    <a href="<?php echo site_url('add'); ?>">Buat Laporan/Komentar</a>
+    <a href="<?php echo base_url().'add'; ?>">Buat Laporan/Komentar</a>
 </div>
 <div class="laporan-terakhir">
     <p>Laporan/Komentar Terakhir</p>
@@ -12,7 +12,7 @@
         <p class="isi"><?php echo $post['isi']; ?></p>
         <br>
         <p class="waktu">Waktu: <?php echo $post['waktu']; ?></p>
-        <a href="<?php echo base_url().'index.php/laporan/'.$post['id'];?>">Lihat Selengkapnya ></a>
+        <a href="<?php echo base_url().'laporan/'.$post['id'];?>">Lihat Selengkapnya ></a>
         <hr>
     <?php endforeach;?>
 </div>
