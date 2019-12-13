fc644f21db3dcbfc66369c2bf6dd8621ff919496
GabrellaMarlikaPutri <gabyaja500@gmail.com>
2019-12-13 22:51:54 +0700

    Memperbaiki controller untuk memanggil bagian komentar

--- CodeIgniter/application/controllers/Komentar.php
+++ CodeIgniter/application/controllers/Komentar.php
@@ -7,7 +7,7 @@ class Komentar extends CI_Controller{
     }
 
     public function index(){
-        $data['judul']="Tambah Komentar/Laporan";
+        $data['title']="Tambah Komentar/Laporan";
         $this->load->view('templates/header_komentar', $data);
         $this->load->view('komentar/index.php', $data);
         $this->load->view('templates/footer');
@@ -15,8 +15,20 @@ class Komentar extends CI_Controller{
         if(isset($_POST['submit'])){
             $this->Md_komentar->tambahKomentar();
             echo "<script>alert('Report / Komentar berhasil ditambahkan!');</script>";
+        }   
+    }
+
+    public function uploadFile(){
+        $config['upload_path'] = base_url('Dokumen/file_upload');
+        $config['max_size'] = 10000;
+        $config['max_size'] = "gif|jpg|jpeg|png|pdf|doc|docx|xls|xlsx|ppt|pptx";
+
+        $this->load->library('upload', $config);
+        if($this->upload->do_upload('lampiran')){
+            echo "<script>alert('Berhasil Upload');</script>";
+        }else{
+            echo "<script>alert('Gagal Upload');</script>";
         }
-        
     }
 }
 ?>
