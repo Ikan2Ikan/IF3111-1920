15d598980e4911df99e564b69a6d4ce84180a993
telagamuhamad <telagamuhamad@gmail.com>
2019-12-13 10:46:55 +0700

    tampilan Home dengan format PHP

---
+++ application/views/buatlapor.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LAPOR!</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="boxlapor">
+		<h1 align="center">SIMPLE LAPOR!</h1>
+		<div class="lapor">
+		<p>Buat Laporan/Komentar</p><hr>
+		</div>
+		<form>
+			<textarea cols="30" rows="5"><h3>Laporan/Komentar</h3></textarea>
+		</form>											
+	</div>
+</body>
+</html>	
