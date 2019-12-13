5a297f3615370ec53e50005fb40f70a5427de7d6
GabrellaMarlikaPutri <gabyaja500@gmail.com>
2019-12-13 19:26:57 +0700

    Menambahkan model untuk tambah komentar

---
+++ CodeIgniter/application/models/Md_komentar.php
@@ -0,0 +1,48 @@
+<?php
+class Md_komentar extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+        date_default_timezone_set('Asia/Jakarta');
+    }
+
+    public function tambahKomentar(){
+        $data = [
+            'id' => '',
+            'waktuPengiriman' => date('d M Y | H:i:s'),
+            'judul_komentar' => $this->input->post('judul_komentar'),
+            'komentar' => $this->input->post('komentar'),
+            'kategori' => $this->input->post('kategori'),
+            'file' => $this->fileUpload()
+        ];
+        $this->db->insert('komentar', $data);
+    }
+
+    public function tampilDaftarKomentar(){
+        $data = $this->db->query("SELECT * FROM komentar")->result_array();
+        return $data;
+    }
+
+    public function tampilDetailKomentar($id){
+        $data = $this->db->query("SELECT * FROM komentar WHERE id=$id")->result_array()[0];
+        return $data;
+    }
+
+    public function fileUpload(){
+        $config = [
+            'upload_path' => './Dokumen/file_upload',
+            'allowed_types' => "gif|jpg|jpeg|png|pdf",
+            'overwrite' => TRUE,
+            'max_size' => "2048000",
+            'max_height' => "768",
+            'max_width' => "1024",
+            'file_name' =>  $this->input->post('lampiran') . "_" . date('dmY-His')
+        ];
+
+        $this->load->library('upload', $config);
+        $this->upload->do_upload('lampiran');
+        print_r($this->upload->display_errors());
+
+        return $this->upload->data('file_name');
+    }
+}
+?>
