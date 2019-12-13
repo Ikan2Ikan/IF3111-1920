795e978e152a5d564caedf8b132adf45c4345694
achmadsyafriyal <achmadsyafriyal@gmail.com>
2019-12-13 22:15:49 +0700

    saya menambahkan tampilan halaman ke 3 dan menambahkan css pada tampilan ke 3 dan ke 2 dam menambahkan foto

---
+++ halaman1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>TUGAS BESAR pabw </title>
+    <link rel="stylesheet" type="text/css" href="css/1css.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  
+  <div id="header">
+    <h1 align="center">LAPOR !!!</h1> 
+      <form action="" method="POST">
+        <input type="text" name="text" placeholder="Cari.." class="cari">
+        <button class="button"><img src="gambar/loupe_78956.png" width="13px">Search</button>
+    <p hr>Buat Laporan/Komentar <button><a href=""><img src="gambar/plus_icon-icons.com_69476.png" width="15px"></a></button></p>
+      </form>
+    <div class="isi1"> 
+      <h4>Laporan/Komentar Terakhir</h4>
+      <hr>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+  </div>
+  <div class="isi2">
+    <h4>Lampiran: Gambar.jpg<span>(Waktu) <a href="">Lihat Selengkapnya></a></span></h4>
+    <hr>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+  </div>
+  <div class="isi3">
+    <h4>Lampiran: Gambar.jpg<span>(Waktu) <a href="">Lihat Selengkapnya></a></span></h4>
+    <hr>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+  </div>
+  <div class="isi4">
+    <h4>Lampiran: Gambar.jpg<span>(Waktu) <a href="">Lihat Selengkapnya></a></span></h4>
+    <hr>
+  </div>
+  </div>
+
+</body>
+</html>
---
+++ halaman2.php
@@ -0,0 +1,34 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+<title> TUGAS BESAR PABW </title>
+<link rel="stylesheet" type="text/css" href="css/2css.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div id="box">
+	<header>
+	<div class="header">
+  <h1>LAPOR !!!</h1>
+	</div>
+	<br><br>
+	</header>
+	<div class="isi">
+  <a > Buat Lapor/Komentar </a><hr>
+   <textarea  name="komentar" id="message" rows="20" cols="131"></textarea> 
+   <br><br>
+   <select name="aspek_komentar">
+      <option >Pilih Aspek Komentar</option>
+      <option value="">Ruangan</option>
+      <option value="">E-learning</option>
+      <option value="">Siakad</option>     
+  </select><br>
+    <input type="file" name="file" value=" " class="konten">
+  </div>
+  <footer>
+    <input type="submit" value="BUAT LAPOR!">
+  </footer>
+</div>
+</body>
+</html>
