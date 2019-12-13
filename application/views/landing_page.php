e5691f29e992ca02f3a413c53a5bf2962a6bbf22
Coriza Caesarchakti <47585721+corizacaesar@users.noreply.github.com>
2019-12-13 09:05:02 +0700

    config
    config

---
+++ application/views/landing_page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SIMPLE LAPOR</title>
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css');?>">
+
+  </head>
+  <body>
+    <center>
+      <h1>SIMPLE LAPOR!</h1>
+
+      <!--section search-->
+      <section>
+        a
+      </section>
+      <!--end section search-->
+
+
+    </center>
+  </body>
+</html>
