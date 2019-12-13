72ae7ec31a829a9d4d5a58ff88766a4a79d2e3a9
audinamia <miaaudina1223@gmail.com>
2019-12-13 14:00:51 +0700

    menambahkan upload file dan comment

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,6 +1,4 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Home extends CI_Controller {
     public function index(){
         $this->load->view('V_home');   
---
+++ application/models/Comment.php
@@ -0,0 +1,49 @@
+<?php
+
+class Comment extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+        date_default_timezone_set('Asia/Jakarta');
+    }
+
+    public function tComm(){
+        $data = [
+            'id' => '',
+            'timestamp' => date('d m Y |H:i:s'),
+            'judul_comm' => $this->input->post('judul_comm'),
+            'isi_comm' => $this->input->post('comment'),
+            'lampiran' => $this->uploadfile()
+        ];
+        $this->db->insert('comment', $data);
+    } 
+
+    public function  sComm(){
+        $data = $this->db->query("SELECT * FROM comment")->result_array();
+        return $data;
+    }
+
+    public function detComm(){
+        $data = $this->db->query("SELECT * FROM comment WHERE id=$id")->result_array()[0];
+        return $data;
+    }
+
+    public function uploadfile(){
+        $file = [
+            'fileup' => '',
+            'typefile' => "jpg|png|doc|gif|jpeg|pdf",
+            'overwrite' => TRUE,
+            'size' => "2048000",
+            'height' => "768";
+            'width' => "1024",
+            'filename' => $this->session->userdata('id') .  "-" . data(dmY-His)
+        ];
+
+        $this->load->library('upload', $file);
+        $this->upload->do_upload('lampiran');
+        print_r($this->upload->display_errors());
+        return $this->upload->data('filename');
+    }
+}
+
+
+?>
