e19d807197ca32c6ec17f683d336c305d36d28c8
Dhiko <your@email.com>
2019-12-13 10:57:41 +0700

    add img css file comment

--- lapor-app/application/views/comment/index.php
+++ lapor-app/application/views/comment/index.php
@@ -1,50 +1,55 @@
-<div class="content">
-    <h1>SIMPLE LAPOR!</h1>
-    <!-- Contoh pake fontawesome -->
-    <i class="fa fa-plus-circle" aria-hidden="true"></i>
-    <div class="head">
-        Buat Laporan/Komentar
+<div class="container">
+    <div class="content">
+        <div class="heading-icon">
+            <img src=<?= base_url('assets/img/logoitera.png') ?> alt="ini foto.png">
+            <h1>LAPOR ITERA!</h1>
+        </div>
+
+        <div class="head">
+            Buat Laporan/Komentar
+        </div>
+
+        <hr>
+
+        <form method="post" action="#" enctype="multipart/form-data">
+
+
+            <div class="main">
+                <input type="text" placeholder="Judul Laporan/Komentar" name="comm_title">
+                <br>
+                <textarea name="comment" placeholder="Laporan/Komentar"></textarea>
+            </div>
+
+            <br>
+
+            <div class="aspect">
+                <select name="aspect">
+                    <option default>Pilih Aspek Pelaporan/Komentar</option>
+                    <option value="Dosen">Dosen</option>
+                    <option value="Staff">Staff</option>
+                    <option value="Mahasiswa">Mahasiswa</option>
+                    <option value="Infrastruktur">Infrastruktur</option>
+                    <option value="Pengajaran">Pengajaran</option>
+                </select>
+            </div>
+
+            <br>
+
+            <div class="file">
+                <input class="file_in" type="file" name="lampiran">
+            </div>
+
+            <br>
+
+            <div class="btn_submit">
+                <button type="submit" name="submit">Buat LAPOR!</button>
+            </div>
+        </form>
+
+        <br>
+
+        <br>
+
+        <hr>
     </div>
-    <i class="fa fa-leaf" aria-hidden="true"></i>
-    <hr>
-
-    <form method="post" action="#" enctype="multipart/form-data">
-
-
-        <div class="main">
-            <input type="text" placeholder="Title" name="comm_title">
-            <textarea name="comment" placeholder="Laporan/Komentar"></textarea>
-        </div>
-
-        <br>
-
-        <div class="aspect">
-            <select name="aspect">
-                <option default>Pilih Aspek Pelaporan/Komentar</option>
-                <option value="Dosen">Dosen</option>
-                <option value="Staff">Staff</option>
-                <option value="Mahasiswa">Mahasiswa</option>
-                <option value="Infrastruktur">Infrastruktur</option>
-                <option value="Pengajaran">Pengajaran</option>
-            </select>
-        </div>
-
-        <br>
-
-        <div class="file">
-            <input class="file_in" type="file" name="lampiran">
-        </div>
-
-        <br>
-
-        <div class="btn_submit">
-            <button type="submit" name="submit">Buat LAPOR!</button>
-        </div>
-    </form>
-
-    <br>
-
-    <br>
-
-    <hr>
 </div>
