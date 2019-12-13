d74684b4f64a548f0977edc32a5e07e125504794
GabrellaMarlikaPutri <gabyaja500@gmail.com>
2019-12-13 19:21:09 +0700

    Membuat halaman utama

--- CodeIgniter/application/views/home/index.php
+++ CodeIgniter/application/views/home/index.php
@@ -1,12 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home/index</title>
-</head>
-<body>
-    <h1>Home / index</h1>
-</body>
-</html>
+<div class="container">
+    <div class="form">
+        <input type="text" name="cari" id="cari" placeholder="Masukkan Keyword Pencarian">
+        <button type="submit" name="cari">Cari</button>
+    </div><br>
+    
+    <div class="link_komentar">
+        <a href="<?base_url('komentar')?>">Buat Laporan/Komentar</a>
+    </div>
+    
+    <h4>Laporan/Komentar Terkini</h4>
+    <hr>
+    <?php foreach($result as $x) :?>
+        <div class="laporan">
+            <a>
+                <a href="<?=base_url('home/detail_komentar/') . $x['id']?>"></a>
+            </a>
+
+            <p><?=$x['komentar']?></p>
+
+            <div class="detail">
+                <span>
+                    <span id="lampiran"><?= $d['lampiran'] ?></span>
+                    <span id="waktuPengiriman"><?= $d['waktuPengiriman']?> WIB</span>
+                </span>
+                <span>
+                    <a href="<?=base_url('home/detail_komentar/') . $x['id']?>">Lihat Selengkapnya ></a>
+                </span>
+            </div>
+            <hr>
+        </div>
+    <?php endforeach;?>
+</div>
+<script src="<?base_url('Dokumen/js/pencarian.js')?>"></script>
