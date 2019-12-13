1b01808e7e2bb24960b7f4ef2e5d32b9730e402d
kurniawanedi <koerniawanedi@gmail.com>
2019-12-14 00:18:03 +0700

    Membuat tampilan detail

---
+++ application/views/pages/tampilan_detail.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url('assets/css/style_tampilan_detail.css') ?>" />
+
+	<title>Tampilan Detail</title>
+</head>
+
+<body>
+	<fieldset>
+		<header>
+			<div class="container">
+				<center>
+					<h1>Simple Lapor!</h1>
+				</center>
+			</div>
+		</header>
+
+		<section id="buatlaporan">
+			<div class="container">
+				<h4>Detail Laporan/Komentar</h4>
+			</div>
+		</section>
+
+		<section id="searchlaporan">
+			<div class="container">
+				<hr>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate cumque, doloribus nisi cum dolore
+					obcaecati dignissimos iure vel in et, nobis alias, facilis pariatur minima tempore similique. Saepe,
+					minus magnam!
+				</p>
+			</div>
+		</section>
+
+		<section id="boxes">
+			<div class="container">
+				<h4>Lampiran</h4>
+
+				<img src="#" alt="">
+
+				<div class="box-h">
+					<div class="waktu">
+						<h4>Waktu : </h4>
+					</div>
+				</div>
+				<div class="box-h">
+					<div class="aspek">
+						<h4>Aspek :</h4>
+					</div>
+				</div>
+				<div class="box-h">
+					<div class="hapus">
+						<h4><a href="#">Hapus Laporan/Komentar></a></h4>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</fieldset>
+</body>
+
+</html>
