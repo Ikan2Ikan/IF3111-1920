e3f83f74b28704fe483a6704c00aecb7b4f0fab0
Muhammad Rizki Oktarlis Setia Budi <mrizki.14115032@student.itera.ac.id>
2019-12-13 22:33:18 +0700

    upload file tentang lapor

--- application/views/tentang_lapor.php
+++ application/views/tentang_lapor.php
@@ -11,15 +11,180 @@
 </head>
 
 <body>
+    <nav>
+        <div class="logo">
+            <h4>Lapor!</h4>
+        </div>
+        <ul class="nav-links">
+            <li>
+                <a href="#">Tentang lapor!</a>
+            </li>
+            <li>
+                <a href="#">Laporan</a>
+            </li>
+            <li>
+                <a href="#">Masuk</a>
+            </li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </nav>
     <section class="intro">
         <div class="inner">
             <div class="content">
-                <h1>LAPORKAN MASALAHMU!!</h1>
+                <h1 class="tittle">LAPORKAN MASALAHMU!!</h1>
                 <a href="#" class="btn">Lanjutkan</a>
             </div>
         </div>
     </section>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum repellat quos et, enim totam corrupti fugit voluptates ut porro. Ab veritatis illo harum alias ad corporis rerum aspernatur labore beatae!</p>
+    <div class="post post-complaint" data-complaint-id="5010653" data-request-data="complaint_id : 5010653">
+        <div class="row-flex">
+            <div class="complaint-head hidden-xs">
+                <div class="complaint-avatar">
+                    <img src="https://storage.lapor.go.id/app/uploads/public/5d2/d24/c4c/thumb_124081_60_60_0_0_crop.jpg" alt="" class="img-responsive img-circle">
+                </div>
+            </div>
+
+            <div class="complaint-body">
+                <div class="complaint-user">
+                    <div class="user-information">
+                        <div class="meta-date" title="" data-toggle="tooltip" data-original-title="Terakhir diupdate 2 menit yang lalu">8 Agu, 08:45</div>
+
+                        <span class="text-user">
+                            <a href="https://www.lapor.go.id/profil/10703">Muhammad Najibulloh</a>
+                        </span>
+
+
+                        <br class="visiible-xs">
+
+                        <span class="text-muted text-channel">
+                            <i class="fa fa-fw fa-globe"></i> Website
+                        </span>
+
+                        <span class="text-status" data-toggle="tooltip" title="" style="color: #FF9800" data-original-title="">
+                            <i class="fa fa-refresh fa-fw"></i>
+
+                            Ditindaklanjuti oleh Instansi
+
+                        </span>
+
+
+                        <span class="text-tracking">
+                            Selesai otomatis dalam 10 hari
+                        </span>
+                    </div>
+                </div>
+
+                <div class="complaint-track-body mg-t-10 mg-b-10 hidden-xs">
+                    <p>
+                        Terdisposisi ke <a href="https://www.lapor.go.id/instansi/dinas-perhubungan-kabupaten-tulungagung"><b>Dinas Perhubungan Kabupaten Tulungagung</b></a>
+                    </p>
+                </div>
+
+                <div class="complaint-title h4">
+                    <a href="https://www.lapor.go.id/laporan/detil/pengeloaan-parkir-2">
+                        Pengeloaan Parkir
+                    </a>
+                </div>
+
+                <div class="complaint-excerpt">
+                    <p class="readmore" style="max-height: none;">Yang terhormat pemerintah kabupaten tulungagung, jawa tmur. Bagaimanakah pengelolaan parkir di wilayah gor lembu peteng? Karena tidak ada standar nominal parkir sehingga juru parkir menarik biaya se enaknya saja. Terimakasih</p>
+                </div>
+
+                <div class="complaint-info">
+                    <p>
+                        <a href="https://www.lapor.go.id/laporan/kategori/kategori-keluhan" class="text-muted">Kategori Keluhan</a>
+                    </p>
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="complaint-meta margin-out-sm">
+            <a class="meta-info info-id hidden-xs" href="#">
+                #5010653
+            </a>
+
+
+
+
+            <a href="javascript:void(0);" class="meta-info" data-request="complaintActions::onShowFollowUps" data-request-update="'admin/complaints/followups': '.post-complaint[data-complaint-id=5010653] .loadedcontent'" data-request-success="$('.post-complaint[data-complaint-id=5010653]').addClass('open').find('.meta-info').removeClass('is-active'); $(this).addClass('is-active');" data-attach-loading="" data-request-flash="">
+                <i class="fa fa-exchange fa-fw"></i> Tindak Lanjut <span class="text-muted">4</span>
+            </a>
+
+            <a href="javascript:void(0);" class="meta-info" data-request="complaintActions::onShowComments" data-request-update="'admin/complaints/comments': '.post-complaint[data-complaint-id=5010653] .loadedcontent'" "="" data-request-success=" $('.post-complaint[data-complaint-id=5010653]').addClass('open').find('.meta-info').removeClass('is-active'); $(this).addClass('is-active');" data-attach-loading="" data-request-flash="">
+                <i class="fa fa-comment-o fa-fw"></i> Komentar <span class="text-muted">0</span>
+            </a>
+
+            <a href="javascript:void(0);" class="meta-info meta-like" data-request="complaintActions::onSupport" data-request-update="'complaints/complaint-like': '.complaint-like0'" data-request-flash="" data-attach-loading="">
+                <span class="complaint-like0 ">
+                    <i class="fa fa-thumbs-o-up"></i> Dukung <span class="text-muted"></span>
+                </span></a>
+
+            <a href="javascript:void(0)" class="meta-info" data-request="complaintActions::onShowShare" data-request-update="'complaints/complaint-share': '.post-complaint[data-complaint-id=5010653] .loadedcontent'" "="" data-request-success=" $('.post-complaint[data-complaint-id=5010653]').addClass('open').find('.meta-info').removeClass('is-active'); $(this).addClass('is-active');" data-attach-loading="" data-request-flash="">
+                <i class="fa fa-share fa-fw"></i> Bagikan
+            </a>
+
+
+
+
+        </div>
+
+        <div class="loadedcontent margin-out-sm"></div>
+    </div>
+    <p>Pengelolaan pengaduan pelayanan publik di setiap organisasi penyelenggara di Indonesia belum terkelola secara efektif dan terintegrasi. Masing-masing organisasi penyelenggara mengelola pengaduan secara parsial dan tidak terkoordinir dengan baik. Akibatnya terjadi duplikasi penanganan pengaduan, atau bahkan bisa terjadi suatu pengaduan tidak ditangani oleh satupun organisasi penyelenggara, dengan alasan pengaduan bukan kewenangannya. Oleh karena itu, untuk mencapai visi dalam good governance maka perlu untuk mengintegrasikan sistem pengelolaan pengaduan pelayanan publik dalam satu pintu. Tujuannya, masyarakat memiliki satu saluran pengaduan secara Nasional.</p>
+
+    <p>Untuk itu Pemerintah Republik Indonesia membentuk Sistem Pengelolaan Pengaduan Pelayanan Publik Nasional (SP4N) - Layanan Aspirasi dan Pengaduan Online Rakyat (LAPOR!) adalah layanan penyampaian semua aspirasi dan pengaduan masyarakat Indonesia melalui beberapa kanal pengaduan yaitu website www.lapor.go.id, SMS 1708, twitter @lapor1708 dan aplikasi Android. Lembaga pengelola SP4N-LAPOR! adalah Kementerian Pendayagunaan Aparatur Negara dan Reformasi Birokrasi (Kementerian PANRB) sebagai Pembina Pelayanan Publik, Kantor Staf Presiden (KSP) sebagai Pengawas Program Prioritas Nasional dan Ombudsman Republik Indonesia sebagai Pengawas Pelayanan Publik. LAPOR! telah ditetapkan sebagai Sistem Pengelolaan Pengaduan Pelayanan Publik Nasional (SP4N) berdasarkan Peraturan Presiden Nomor 76 Tahun 2013 dan Peraturan Menteri Pendayagunaan Aparatur Negara dan Reformasi Birokrasi Nomor 3 Tahun 2015.</p>
+
+    <p>SP4N-LAPOR! dibentuk untuk merealisasikan kebijakan “<i>no wrong door policy</i>” yang menjamin hak masyarakat agar pengaduan dari manapun dan jenis apapun akan disalurkan kepada penyelenggara pelayanan publik yang berwenang menanganinya. SP4N bertujuan agar:
+
+    </p>
+    <ul>
+        <li>Penyelenggara dapat mengelola pengaduan dari masyarakat secara sederhana, cepat, tepat, tuntas, dan terkoordinasi dengan baik;</li>
+        <li>Penyelenggara memberikan akses untuk partisipasi masyarakat dalam menyampaikan pengaduan; dan</li>
+        <li>Meningkatkan kualitas pelayanan publik.</li>
+    </ul>
+    <p></p>
+
+    <p>SP4N-LAPOR! telah terhubung dengan 34 Kementerian, 96 Lembaga, dan 493 Pemerintah daerah di Indonesia </p>
+
+    <p class="text-center">
+        <a href="/themes/lapor/assets/images/map-summary.png" target="_blank"><img src="/themes/lapor/assets/images/map-summary.png" alt="summary" class="fr-fic fr-dii">&nbsp;</a>
+    </p>
+
+    <p>Jumlah pelapor per Januari 2019 adalah sebanyak 801.257 pengguna. Total laporan yang telah masuk sebanyak 1.389.891. Sumber laporan terbanyak melalui website diikuti oleh SMS, twitter dan aplikasi mobile</p>
+
+    <p> Fitur-fitur yang ada dalam SP4N-LAPOR!
+    </p>
+    <ol>
+        <li>Anonim: Fitur yang bisa dipilih oleh pelapor yang akan membuat identitas pelapor tidak akan diketahui oleh pihak terlapor dan masyarakat umum.</li>
+        <li>Rahasia: Seluruh isi laporan tidak dapat dilihat oleh publik.</li>
+        <li>Tracking id: Nomor unik yang berguna untuk meninjau proses tindak lanjut laporan yang disampaikan oleh masyarakat</li>
+    </ol>
+    <p></p>
+
+    <p class="text-center"><strong>“Berani LAPOR! Untuk Pelayanan Publik yang Lebih Baik”</strong></p>
+    </div>
+    <script src="<?= base_url(); ?>assets/js/app.js"></script>
+
+    <!-- footer -->
+    <div class="footer">
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h1 class="logo-text">Lapor!</h1>
+            </div>
+            <div class="footer-section link"></div>
+            <div class="footer-section contact-form"></div>
+        </div>
+
+        <div class="footer-bottom">
+            &copy; Pejuang Tubes
+        </div>
+    </div>
 </body>
 
 </html>
