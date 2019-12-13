28ee69626456ebb3b5307454172ba5faaae472f3
Tasya Karinda <karindachan@users.noreply.github.com>
2019-12-13 19:51:53 +0700

    menambahkan detail

---
+++ application/views/detail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+ <body>
+  <h1>SIMPLE LAPOR</h1>
+  <form action = "post">
+    <input type = "text" name = "judul" placeholder="Buat laporan/komentar">
+  </form>
+  <textarea type="text" name="laporan" rows="13" cols="97" placeholder="Buat laporan/komentar">
+  </textarea>
+  <form>
+    <input type="file" name="image" accept="image/"
+  </form>
+ <button>Lapor!</button>
+ </body>  
+</html>
