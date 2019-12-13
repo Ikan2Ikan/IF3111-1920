17e8767665437a69ccfc4182dc70e2548b41e355
Tasya Karinda <karindachan@users.noreply.github.com>
2019-12-13 14:43:38 +0700

    menambahkan landing page 1.4

--- application/views/V_home.php
+++ application/views/V_home.php
@@ -3,6 +3,25 @@
 
 <style>
 
+.kolom-lapor {
+      align: center; 
+      width: 700px;
+      height: 200px;
+      padding: 10px;
+      border: 1px black;
+      margin-left: 300px;   
+      background-color: pink;
+    }
+
+.kolom-lapor1{
+      width: 700px;
+      height: 50px;
+      padding: 10px;
+      border: 1px black;
+      margin-left: 300px;   
+      background-color: pink;
+    }
+
 </style>
 
 <head>
@@ -14,26 +33,28 @@
 
 <body>
 
-<nav>
 <div>
    <label>LAPOR! ITERA</label>
     <a href="/home/">Tentang LAPOR!</a>
     <a href="/gallery/">Laporan</a>
-    <a href="/contact/">Masuk</a>
-    <button>Daftar</button>
-    <form>
-	<input class="search" type="text" placeholder="Cari..." required>	
-	<input class="button" type="button" value="Cari">		
-    </form>
+    <a href="#"><button>Masuk</button></a>
+    <a href="#"><button>Daftar</button></a>
+    <form align = "right"><input class="search" type="text" placeholder="Cari Aduan" required></form>
 </div>
-</nav>
 
 <div class = "kepala">
 <h1 align= "center">Layanan Aspirasi Online Masyarakat ITERA</h1>
 <h3 align= "center">Sampaikan laporan Anda langsung kepada instansi kampus berwenang</h3>
+<br>
 </div>
 
 <div class = "kolom-lapor">
+<textarea type="text" name="laporan" rows="13" cols="92" placeholder="Test"></textarea>
+</div>
+
+<div class= "kolom-lapor1">
+<button float="right;">Lapor!</button><br>
+
 </div>
 
 </body>
