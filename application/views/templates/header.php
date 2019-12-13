e7376465a03d1198337b4d14fef038327a95f791
rivaldofez <rivaldofez@gmail.com>
2019-12-14 00:56:55 +0700

    Mengkoneksikan header, footer dan home ke css

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php echo base_url('asset/css/style.css')?>">
-    <title>Home | Lapor</title>
+    <title><?php echo $judul; ?> | Lapor</title>
 </head>
 
 <body>
