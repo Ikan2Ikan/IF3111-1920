7c1094a999f2d07fa15992c94295bcec074a6d3f
erawati14117070 <hikariaera@gmail.com>
2019-12-13 06:12:58 -0800

    menambahkan css

--- application/views/V_home.php
+++ application/views/V_home.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('asset/home.css')?>">
     <title>LAPOR! ITERA</title>
 </head>
 
@@ -16,24 +17,27 @@
 
 <nav>
 <div>
+    <form>
    <label>LAPOR! ITERA</label>
     <a href="/home/">Tentang LAPOR!</a>
     <a href="/gallery/">Laporan</a>
-    <a href="/contact/">Masuk</a>
-    <button>Daftar</button>
-    <form>
-	<input class="search" type="text" placeholder="Cari..." required>	
+	<input class="search" type="text" placeholder="Cari Aduan" required>	
 	<input class="button" type="button" value="Cari">		
     </form>
 </div>
 </nav>
 
 <div class = "kepala">
-<h1 align= "center">Layanan Aspirasi Online Masyarakat ITERA</h1>
-<h3 align= "center">Sampaikan laporan Anda langsung kepada instansi kampus berwenang</h3>
+<h1>Layanan Aspirasi Online Masyarakat ITERA</h1>
+<h3>Sampaikan laporan Anda langsung kepada instansi kampus berwenang</h3>
 </div>
 
 <div class = "kolom-lapor">
+<textarea type="text" name="laporan" rows="13" cols="97" placeholder="Ketik laporan Anda..."></textarea>
+</div>
+
+<div class= "kolom-lapor1">
+<input class="button" type="button" value="Lapor!">
 </div>
 
 </body>
