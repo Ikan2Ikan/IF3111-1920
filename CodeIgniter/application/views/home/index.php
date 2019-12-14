6338192b0efec1816b3c75a59d5ddea70206c0ab
rikalaila <rika.14117024@student.itera.ac.id>
2019-12-14 07:40:47 +0700

    menambahkan fungsi hapus

--- CodeIgniter/application/views/home/index.php
+++ CodeIgniter/application/views/home/index.php
@@ -5,27 +5,26 @@
 	</form>
 
 	<br>
-	<a style="text-align: center;" href="<?php echo site_url('home/lapor') ?>">Buat Laporan/Komentar</a>
+	<a style="text-align: center;" href="<?= base_url(); ?>home/lapor">Buat Laporan/Komentar</a>
 	<br>
 
 	<label class="lampiran">Lampiran/Komentar Terakhir</label>
 	<hr>
 	<?php 
 		foreach($data as $tampil){ 
-
-		$string = $tampil->laporan; 
-		if (strlen($string) > 150) {
-			$trimstring = substr($string, 0, 300). '.....';
-		} else {
-			$trimstring = $string;
-		}
-		echo $trimstring;
+			$string = $tampil->laporan; 
+			if (strlen($string) > 150) {
+				$trimstring = substr($string, 0, 150). '.....';
+			} else {
+				$trimstring = $string;
+			}
+			echo $trimstring;
 	?>	
 	<p>
 		Lampiran: <?= $tampil->file?>
 		<span>
 			<a> Waktu: <?= $tampil->date?>  </a>  
-			<a href="<?= site_url('home/lihat')?>">Lihat selengkapnya</a>
+			<a href="<?= base_url(); ?>home/lihat/<?= $tampil->id;?>">Lihat selengkapnya</a>
 		</span>
 	</p>
 	<hr>	
---
+++ CodeIgniter/application/views/home/lihat.php
@@ -0,0 +1,16 @@
+
+	<p>Detail Laporan/Komentar</p>
+	<hr>
+
+	<?= $data['laporan'] ?>
+	<br><br>
+
+	Lampiran:
+	<?= $data['file'] ?>
+	<br><br>
+
+	Waktu: <?= $data['date']?>  &nbsp &nbsp &nbsp
+	Aspek: <?= $data['aspek']?> 
+
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+	<a href="<?= base_url(); ?>home/hapus/<?= $data['id']?>">Hapus Laporan/Komentar</a>
