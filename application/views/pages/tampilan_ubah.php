07bd1bbabcf5ee8f8e15207cf54fdc7945ac3d3e
kurniawanedi <koerniawanedi@gmail.com>
2019-12-14 13:00:55 +0700

    Membuat tampilan_ubah.php

---
+++ application/views/pages/tampilan_ubah.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url('assets/css/style_tampilan_ubah.css') ?>" />
+	<title>Tampilan Ubah</title>
+</head>
+
+<body>
+	<fieldset>
+		<header>
+			<div class="container">
+				<center>
+					<h1>Simple Lapor!</h1>
+				</center>
+			</div>
+		</header>
+		<section id="buatlaporan">
+			<div class="container">
+				<h3>Buat Laporan/Komentar</h3>
+				<hr>
+				<br>
+			</div>
+		</section>
+
+		<section id="formbuatlaporan">
+			<div class="container">
+
+				<?php echo form_open("control/ubah/" . $laporan->id); ?>
+				<form action="tambah" method="POST" enctype="multipart/form-data">
+
+
+					<textarea name="laporan" id="laporan" cols="77" rows="20" placeholder="Laporan/Komentar" required><?php echo set_value('laporan', $laporan->laporan) ?></textarea>
+					<br><br>
+					<label for="">Pilih Aspek Pelaporan/Komentar</label>
+					<br>
+					<select name="aspek" id="aspek">
+						<option value="Dosen">Dosen</option>
+						<option value="Staf">Staff</option>
+						<option value="Mahasiswa">Mahasiswa</option>
+						<option value="Infrastruktur">Infrastruktur</option>
+						<option value="Pengajaran">Pengajaran</option>
+					</select>
+					<br>
+					<br>
+
+					<input type="file" name="lampiran" id="" required>
+					<br><br>
+					<button type="submit" class="button-1" name="submit" value="simpan">Ubah LAPOR!</button>
+
+				</form>
+				<br>
+				<hr>
+			</div>
+		</section>
+	</fieldset>
+</body>
+
+</html>
