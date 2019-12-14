1664431ae23a77d09221da79ad4ab0e666f2fd1b
erawati14117070 <hikariaera@gmail.com>
2019-12-13 17:16:57 -0800

    menambahkan tampilan

---
+++ application/views/V_detail.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>LAPOR! ITERA</title>
+</head>
+
+<body>
+    <div class="container">
+        <div>
+            <h1 class="judul">LAPOR! ITERA</h1>
+        </div>
+
+        <div>
+            <p class="laporan">Detail Laporan/Komentar</p>
+        </div>
+
+        <hr>
+        </hr>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi at tempora eius dolorem assumenda libero, laboriosam deserunt animi illo earum asperiores quos nisi repellendus rerum vel maiores eum cupiditate enim?</p>
+
+    </div>
+</body>
+
+</html>
