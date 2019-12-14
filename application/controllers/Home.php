961311461c5023a5c22401134cc4ae6eef98cb70
erawati14117070 <hikariaera@gmail.com>
2019-12-13 17:14:43 -0800

    menambahkan metod

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,29 +1,33 @@
 <?php
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('Comment');
     }
 
-    public function index(){
-        
+    public function index()
+    {
+
         $data['judul'] = "Halaman Lapor";
         $this->load->view('templates/coment', $data);
         $this->load->view('V_simple');
-        $this->load->view('V_home',$data);
+        $this->load->view('V_home', $data);
+        $this->load->view('V_detail');
         $this->load->view('templates/footer', $data);
-       
     }
 
 
-    public function fileUpload(){
+    public function fileUpload()
+    {
         $file['fileup'] = base_url('assets/file');
         $file['typefile']  = 'jpg|png|doc|gif|jpeg|pdf';
         $file['size'] = 5000;
 
         $this->load->library('upload', $file);
-        if ($this->upload->do_upload('lampiran')){
+        if ($this->upload->do_upload('lampiran')) {
             echo "<script>
             alert('berhasil di upload');
             </script>";
@@ -33,5 +37,4 @@ class Home extends CI_Controller {
             </script>";
         }
     }
-    
 }
