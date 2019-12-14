ecfad068a4c56348a8c00fa1ce99c4a19266530d
rikalaila <rika.14117024@student.itera.ac.id>
2019-12-14 07:39:19 +0700

    menambahkan fungsi hapus

--- CodeIgniter/application/models/ambil_data.php
+++ CodeIgniter/application/models/ambil_data.php
@@ -8,4 +8,16 @@ class ambil_data extends CI_Model{
 	function input_data($data,$table){
 		$this->db->insert($table,$data);
 	}
+
+	function hapuslaporan($id){
+		$this->db->where('id',$id);
+		$this->db->delete('laporan');
+	}
+
+	public function Laporandetail($id){
+	    $data = $this->db->query("select * from laporan where id=$id")->result_array()[0];
+	    return $data;
+ 	}
 }
+
+
