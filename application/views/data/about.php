50a23da794a55d7408886281f9878ec1421ddde4
Afifah Dwikirani <47997108+AfifahDk@users.noreply.github.com>
2019-12-14 22:30:19 +0700

    Tubes PABW 14117105-AFIFAH DWIKIRANI & 14117105-PUNGKI RESTI
    pada branch TubesKamii yang telah saya upload perubahan serta program fixnya tidak bisa dilakukan pull request

---
+++ application/views/data/about.php
@@ -0,0 +1,26 @@
+<fieldset>
+<div class="judul">
+<h1>LAPOR in Aja!</h1>
+</div>
+<form action="" method="post">
+<br/>
+<br/>
+Kali ini kami ditugaskan akan membuat aplikasi web seperti lapor untuk
+mengumpulkan laporan/komentar terkait layanan di program studi teknik 
+informatika ITERA.
+
+Web ini kami beri nama LAPOR in AJA!
+
+Perihal tampilan, kami mohon maaf karena sangat diluar dari kata sempurna.
+
+Terimakasih
+<hr/>
+<br/>
+<br/>
+
+<p> Anggota Kelompok</p>
+<p>1. Afifah Dwikirani  14117105</p>
+<p>2. Pungki Resti P.   14117154</p>
+<p>Pemrograman Berbasis Web RC</p>
+</form>
+</fieldset>
---
+++ application/views/data/tambah.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<head>
+    <title>Laporan</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/style.css">
+</head>
+<body>
+<fieldset>    
+        <h1 class="judul">LAPOR in AJA!</h1>
+<?= validation_errors();?>
+<div class="tambah">
+<form action="" method="post" enctype="multipart/form-data"> 
+
+    <p>Silahkan ketikkan laporan/komentar yang ingin anda ajukan</p>
+    <p>Laporan/komentar dapat berupa pernyataan atau pertanyaan, dan hanya dapat dilakukan berdasarkan aspek-aspek dibawah ini</p>
+    <hr/>
+    <label for="lapor">Buat Laporan/Komentar</label></br>
+    <hr />             
+    <!--Laporan/Komentar-->
+    <textarea name="lapor" id="lapor" placeholder="Laporan/Komentar" row="10" cols="100"></textarea>
+    <br><br>
+    <div class="aspek">
+        <select id="aspek" name="aspek">
+            <option>Pilih Aspek Pelaporan/Komentar</option>
+            <option>Administrasi</option>
+            <option>Biaya Iuran</option>
+            <option>BUMN
+            <option>BPJS</option>
+            <option>Cara pengajuan kecelakaan kerja</option>
+            <option>Data dan Informasi Umum</option>
+            <option>Energi dan Sumber Daya Alam</option>
+            <option>Fasilitas Umum</option>
+            <option>Gangguan aplikasi online</option>
+            <option>Harga Pasar</option>
+            <option>Informasi Nomor kartu</option>
+            <option>Infrastruktur</option>
+            <option>investasi MIGAS</option>
+            <option>Jam Operasional Kantor</option>
+            <option>Keamanan dan ketertiban Masyarakat</option>
+            <option>Keluhan Masyarakat</option>
+            <option>Lembaga Ombudsman</option>
+            <option>Pariwisata</option>
+            <option>Pensiunan</option>
+            <option>Reformasi Birokrasi</option>
+            <option>Situasi Khusus</option>
+            <option>Kebencanaan</option>
+        </select>
+        </select>
+    </div>
+        <div class="upload">
+        <input type="file" name="gambar" id="gambar">
+        </div>
+        <br>
+        <div class="tombol">
+            <input type="submit" name="tambah" value="Buat LAPOR!">
+        </div>
+        <hr />
+  
+ </form>
+ </div>
+</fieldset>
+</body>
