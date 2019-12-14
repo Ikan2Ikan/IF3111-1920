0a1af98be8aadac0a93851cac11b6326cb27e887
kurniawanedi <koerniawanedi@gmail.com>
2019-12-14 12:56:50 +0700

    menambahkan control untuk bisa crud

--- application/controllers/Control.php
+++ application/controllers/Control.php
@@ -9,29 +9,66 @@ class Control extends CI_Controller
         parent::__construct();
         $this->load->model("control_model");
         $this->load->helper(array('form', 'url'));
-        // $this->load->library('form_validation');
     }
 
+    // menampilkan semua data ================================================
     public function index()
     {
-        // $data["pages"] = "tampilan_utama";
-        $this->load->view("pages/tampilan_utama");
+        $data['laporan'] = $this->control_model->read();
+        $this->load->view("pages/tampilan_utama", $data);
     }
 
+    // menampilkan data yang dipilih =========================================
+    public function lihat($id)
+    {
+        $data['laporan'] = $this->control_model->view_by($id);
+        $this->load->view('pages/tampilan_detail', $data);
+    }
+
+    // menambahkan data ======================================================
+    public function tambah()
+    {
+        $upload = $this->control_model->upload();
+        $this->control_model->create($upload);
+
+        redirect('control');
+    }
+
+    // mengubah data ========================================================
+    public function ubah($id)
+    {
+        $data['laporan'] = $this->control_model->view_by($id);
+        $this->load->view('pages/tampilan_ubah', $data);
+
+        if ($this->input->post('submit')) {
+            $upload = $this->control_model->upload();
+            $this->control_model->update($upload, $id);
+            redirect('control');
+        }
+    }
+
+    // menghapus data yang dipilih ==========================================
+    public function hapus($id)
+    {
+        $this->control_model->delete($id);
+        redirect('control');
+    }
+
+    // load halaman tampilan_lapor ==========================================
     public function tampilan_lapor()
     {
-        //$data["pages"] = "tampilan_lapor";
         $this->load->view("pages/tampilan_lapor");
     }
+
+    // load halaman tampilan_detail =========================================
     public function tampilan_detail()
     {
         $this->load->view("pages/tampilan_detail");
     }
 
-    public function tambah()
+    // load halaman tampilan_ubah =========================================
+    public function tampilan_ubah()
     {
-        $data = array();
-        $upload = $this->control_model->upload();
-        $this->control_model->create($upload);
+        $this->load->view("pages/tampilan_ubah");
     }
 }
