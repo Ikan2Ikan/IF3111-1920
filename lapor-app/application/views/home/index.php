117fe63ec5c4f6fa698de892f5360e389ad60654
Dhiko <your@email.com>
2019-12-14 13:59:56 +0700

    update css not loggin yet

--- lapor-app/application/views/home/index.php
+++ lapor-app/application/views/home/index.php
@@ -3,57 +3,61 @@
 // var_dump($_POST['keyword']);
 ?>
 <div class="container">
-
-  <div class="log-reg">
-    <a href="<?= base_url('auth') ?>">Login</a>
-    <a href="<?= base_url('auth/register') ?>">Register</a>
-  </div>
-
-  <div class="heading-icon">
-    <img src=<?= base_url('assets/img/logoitera.png') ?> alt="ini foto.png">
-    <h1>LAPOR ITERA!</h1>
-  </div>
-
-  <div class="form">
-    <input type="text" name="keyword" id="keyword" aria-describedby="helpId" placeholder="Masukkan Keyword">
-    <button type="submit" name="cari">Cari</button>
-  </div>
-
-  <br>
-
-  <div class="com_link">
-    <a href="<?= base_url('comment') ?>">Buat Laporan/Komentar </a>
-    <i class="fa fa-plus-circle" aria-hidden="true"></i>
-  </div>
-
-  <h3>Laporan/Komentar Terkini</h3>
-  <hr>
-
-  <?php foreach ($result as $d) :
-    ?>
-    <div class="laporan">
-      <a>
-        <a href="<?= base_url('home/commentDetail/') . $d['comm_id'] ?>"><?= $d['comm_title'] ?></a>
-        <small>Pelapor : <?= $d['fullname'] ?></small>
-      </a>
-
-      <p><?= $d['comm'] ?></p>
-
-      <div class="details">
-        <span>
-          <span id="lampiran"><?= $d['lampiran'] ?> </span>
-          <span id="timestamp">| <?= $d['timestamp'] ?> WIB</span>
-        </span>
-
-        <span>
-          <a href="<?= base_url('home/commentDetail/') . $d['comm_id'] ?>">Selengkapnya</a>
-        </span>
-      </div>
-
-      <hr>
+  <div class="container1">
+    <div class="log-reg">
+      <a href="<?= base_url('auth') ?>">Login</a>
+      <a href="<?= base_url('auth/register') ?>">Register</a>
     </div>
-  <?php endforeach;
-  ?>
+
+    <div class="heading-icon">
+      <img src=<?= base_url('assets/img/logoitera.png') ?> alt="ini foto.png">
+      <h1>LAPOR ITERA!</h1>
+    </div>
+
+    <div class="form">
+      <input type="text" name="keyword" id="keyword" aria-describedby="helpId" placeholder="Masukkan Keyword">
+      <button type="submit" name="cari">Cari</button>
+    </div>
+
+    <br>
+
+    <div class="com_link">
+      <a href="<?= base_url('comment') ?>">Buat Laporan/Komentar </a>
+      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+    </div>
+
+  </div>
+
+  <div class="container2">
+    <h4>Laporan/Komentar Terkini</h4>
+    <hr>
+
+    <?php foreach ($result as $d) :
+      ?>
+      <div class="laporan">
+        <a>
+          <a href="<?= base_url('home/commentDetail/') . $d['comm_id'] ?>"><?= $d['comm_title'] ?></a>
+          <small>Pelapor : <?= $d['fullname'] ?></small>
+        </a>
+
+        <p><?= $d['comm'] ?></p>
+
+        <div class="details">
+          <span>
+            <span id="lampiran"><?= $d['lampiran'] ?> </span>
+            <span id="timestamp">| <?= $d['timestamp'] ?> WIB</span>
+          </span>
+
+          <span>
+            <a href="<?= base_url('home/commentDetail/') . $d['comm_id'] ?>">Selengkapnya</a>
+          </span>
+        </div>
+
+        <hr>
+      </div>
+    <?php endforeach;
+    ?>
+  </div>
 
 </div>
 <script src="<?= base_url('assets/js/liveSearch.js') ?>"></script>
