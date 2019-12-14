1003a25d53947680a254029050d62c01a4d3272d
Dian Asmara Dahana <50095597+dianasmrdhn@users.noreply.github.com>
2019-12-14 02:48:11 -0800

    Upload FIle model fix
    Penambahan fitur baca daftar laporan, input laporan

--- Lapor/application/models/Auth_model.php
+++ Lapor/application/models/Auth_model.php
@@ -6,19 +6,23 @@ class Auth_model extends CI_Model
 {
     public $table       = 'userlogin';
 
-    public function cari_Like($inp)
-    {
-        $this->db->order_by('tanggal', 'desc');
-        return $this->db->like('isi', $inp);
+    public function Baca($data) {
+        return $this->db->get_where('komentar', $data);
     }
+
     public function inputKomentar($data)
     {
         $this->db->insert('komentar', $data);
     }
 
-    public function tampilLapor()
-    {
+    public function tampilLapor() {
         $this->db->order_by('tanggal', 'desc');
         return $this->db->get('komentar');
     }
+
+    public function cari_Like($inp) {
+        $this->db->order_by('tanggal', 'desc');
+        return $this->db->like('isi', $inp);
+    }
+
 }
