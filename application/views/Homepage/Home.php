85ec61a415f6f7285d5b3870290e2bd6f223f9cb
Muhammad Rizki Oktarlis Setia Budi <mrizki.14115032@student.itera.ac.id>
2019-12-14 18:22:44 +0700

    update view Homepage

--- application/views/Homepage/Home.php
+++ application/views/Homepage/Home.php
@@ -5,69 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/ghome.css" media="screen"> -->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/home.css" media="screen">
     <title>Document</title>
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 14px;
-
-        }
-
-        #judul {
-            color: black;
-            text-align: center;
-        }
-
-        .containerAtas {
-            margin: auto;
-            width: 80%;
-            /* border: 3px solid green; */
-            padding: 10px;
-            text-align: center;
-        }
-
-        input[type=text] {
-            width: 70%;
-            height: 30px;
-        }
-
-        #button1 {
-            background-color: white;
-            color: black;
-            border: 2px solid #555555;
-            width: 10%;
-            height: 35px;
-        }
-
-        .buatLaporan {
-            margin: auto;
-            width: 80%;
-            /* border: 3px solid green; */
-            padding: 10px;
-            text-align: center;
-        }
-
-        #button2 {
-            background-color: white;
-            color: black;
-            border: 2px solid #555555;
-            width: 10%;
-            height: 35px;
-        }
-
-        .cKomentar {
-            margin: auto;
-            width: 94%;
-            /* border: 3px solid green; */
-            padding: 10px;
-            text-align: left;
-        }
-
-        .notFound {
-            font-weight: bold;
-        }
-    </style>
 </head>
 
 <body>
