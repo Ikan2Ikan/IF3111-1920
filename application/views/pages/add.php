e3e6b603ec4b79e3caf7be7f6c805ed0f6b57ef5
Jefri Manurung <jefrymanurung23@gmail.com>
2019-12-14 22:44:38 +0700

    ui/ux

--- application/views/pages/add.php
+++ application/views/pages/add.php
@@ -1,26 +1,32 @@
-<div class="container">
-	<div class="buat-laporan">
-		<!-- <p>Buat Laporan/Komentar</p>
-		<hr> -->
-		<?php echo form_open_multipart('laporan/input'); ?>
-			<textarea placeholder="Ketik Laporan Anda..." name="isi-laporan"></textarea>
+<section class="about" id="buat-lapor">
+	<div class="container">
+		<div class="alas">
+			<div class="buat-laporan">
+				<!-- <p>Buat Laporan/Komentar</p>
+				<hr> -->
+				<?php echo form_open_multipart('laporan/input'); ?>
+					<textarea placeholder="Ketik Laporan Anda..." name="isi-laporan"></textarea>
 
-			<div class="custom-select">
-				<select name="aspek">
-					<option hidden>Pilih Aspek Pelaporan/Komentar</option>
-					<option value="Dosen">Dosen</option>
-					<option value="Staff">Staff</option>
-					<option value="Mahasiswa">Mahasiswa</option>
-					<option value="Infrastruktur">Infrastruktur</option>
-					<option value="Pengajaran">Pengajaran</option>
-				</select>
+					<div class="custom-select">
+						<select name="aspek">
+							<option hidden>Pilih Aspek Pelaporan/Komentar</option>
+							<option value="Dosen">Dosen</option>
+							<option value="Staff">Staff</option>
+							<option value="Mahasiswa">Mahasiswa</option>
+							<option value="Infrastruktur">Infrastruktur</option>
+							<option value="Pengajaran">Pengajaran</option>
+						</select>
+					</div>
+					<input type="file" name="lampiran" id="real-file" hidden="hidden" />
+					<button type="button" id="custom-button">Choose File</button>
+					<span id="custom-text">No file chosen, yet.</span>
+					<br>
+
+					<input class="btn px-5" type="submit" name="send" id="send-button" value="LAPOR!">
+				<?php echo form_close(); ?>
 			</div>
-			<input type="file" name="lampiran" id="real-file" hidden="hidden" />
-			<button type="button" id="custom-button">Choose File</button>
-			<span id="custom-text">No file chosen, yet.</span>
-			<br>
-
-			<input type="submit" name="send" id="send-button" value="LAPOR">
-		<?php echo form_close(); ?>
+		</div>
 	</div>
-</div>
+</section>
+
+<script src="<?php echo base_url(); ?>js/script2.js"></script>
--- application/views/pages/edit.php
+++ application/views/pages/edit.php
@@ -1,28 +1,34 @@
-<div class="container">
-	<div class="buat-laporan">
-		<!-- <p>Buat Laporan/Komentar</p>
-		<hr> -->
-		<form action="<?php echo base_url('laporan/update/').$laporan['id']; ?>" method="post">
-			<textarea placeholder="Ketik Laporan Anda..." name="isi-laporan"><?php echo $laporan['isi']; ?></textarea>
+<section class="about" id="buat-lapor">
+	<div class="container">
+		<div class="alas">
+			<div class="buat-laporan">
+				<!-- <p>Buat Laporan/Komentar</p>
+				<hr> -->
+				<form action="<?php echo base_url('laporan/update/').$laporan['id']; ?>" method="post">
+					<textarea placeholder="Ketik Laporan Anda..." name="isi-laporan"><?php echo $laporan['isi']; ?></textarea>
 
-			<div class="custom-select">
-				<select name="aspek">
-                    <?php $default_aspek = $laporan['aspek'] ?>
-					<option hidden>Pilih Aspek Pelaporan/Komentar</option>
-					<option value="Dosen" <?php echo ($default_aspek == "Dosen")?"selected":""; ?>>Dosen</option>
-					<option value="Staff" <?php echo ($default_aspek == "Staff")?"selected":""; ?>>Staff</option>
-					<option value="Mahasiswa" <?php echo ($default_aspek == "Mahasiswa")?"selected":""; ?>>Mahasiswa</option>
-					<option value="Infrastruktur" <?php echo ($default_aspek == "Infrastruktur")?"selected":""; ?>>Infrastruktur</option>
-					<option value="Pengajaran" <?php echo ($default_aspek == "Pengajaran")?"selected":""; ?>>Pengajaran</option>
-				</select>
+					<div class="custom-select">
+						<select name="aspek">
+							<?php $default_aspek = $laporan['aspek'] ?>
+							<option hidden>Pilih Aspek Pelaporan/Komentar</option>
+							<option value="Dosen" <?php echo ($default_aspek == "Dosen")?"selected":""; ?>>Dosen</option>
+							<option value="Staff" <?php echo ($default_aspek == "Staff")?"selected":""; ?>>Staff</option>
+							<option value="Mahasiswa" <?php echo ($default_aspek == "Mahasiswa")?"selected":""; ?>>Mahasiswa</option>
+							<option value="Infrastruktur" <?php echo ($default_aspek == "Infrastruktur")?"selected":""; ?>>Infrastruktur</option>
+							<option value="Pengajaran" <?php echo ($default_aspek == "Pengajaran")?"selected":""; ?>>Pengajaran</option>
+						</select>
+					</div>
+
+					<input type="file" name="lampiran" id="real-file" hidden="hidden" />
+					<button type="button" id="custom-button">Choose File</button>
+					<span id="custom-text">No file chosen, yet.</span>
+					<br>
+
+					<input class="btn px-5" type="submit" name="send" id="send-button" value="LAPOR">
+				</form>
 			</div>
-
-			<input type="file" name="lampiran" id="real-file" hidden="hidden" />
-			<button type="button" id="custom-button">Choose File</button>
-			<span id="custom-text">No file chosen, yet.</span>
-			<br>
-
-			<input type="submit" name="send" id="send-button" value="LAPOR">
-		</form>
+		</div>
 	</div>
-</div>
+</section>
+
+<script src="<?php echo base_url(); ?>js/script2.js"></script>
