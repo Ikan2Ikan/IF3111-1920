513e40db48cddfc013635fb2c7fc7c0a2fe1defa
imanmaliki11 <45257900+imanmaliki11@users.noreply.github.com>
2019-12-14 17:27:23 +0700

    tambah controler
    baru ada index

---
+++ Lapor/application/controllers/Auth.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->database();
+        $this->load->model('Auth_model');
+    }
+    public function index()
+    {
+        $this->load->view('auth/header');
+
+        if($this->input->post('submit')) {
+            $data['user'] = $this->Auth_model->cari_Like($this->input->post('search'));
+        }
+        
+        $data['user'] = $this->Auth_model->tampilLapor()->result();
+        
+        $this->load->view('auth/home', $data);
+    }
+
+}
