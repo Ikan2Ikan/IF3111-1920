7000409f7cc6f1bc40a23ebc6d18e7427db47825
Latisya <50728971+Latisya@users.noreply.github.com>
2019-12-14 12:25:43 +0700

    fungsi update

--- CodeIgniter/application/models/ambil_data.php
+++ CodeIgniter/application/models/ambil_data.php
@@ -9,15 +9,30 @@ class ambil_data extends CI_Model{
 		$this->db->insert($table,$data);
 	}
 
+	function update_data($where,$data,$table){
+		// $this->db->update($table, $data, array('id' => $this->input->post('id')));
+		// $this->db->where('id', $this->input->post('id'));
+		// $this->db->update($table,$data);
+		$this->db->where($where);
+		$this->db->update($table,$data);
+	}
+
 	function hapuslaporan($id){
 		$this->db->where('id',$id);
 		$this->db->delete('laporan');
 	}
 
-	public function Laporandetail($id){
+	function Laporandetail($id){
 	    $data = $this->db->query("select * from laporan where id=$id")->result_array()[0];
 	    return $data;
  	}
+
+ 	function ubah_id($id){
+	    $data = $this->db->get_where('laporan', ['id' => $id])->row_array();
+	    return $data;
+ 	}
+
+ 	
 }
 
 
