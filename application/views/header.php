b09421268d2eea470075cd1be9a5fee166e2061e
agusmichaelsianipar <agus010sianipar@gmail.com>
2019-12-14 23:53:23 +0700

    Add header and footer for template

---
+++ application/views/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LAPOR !</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/headerstyle.css'); ?>" />
+</head>
+<body>
+<div class="container">
+	<div class="head">
+		<div class="tab"><a href="<?php echo base_url('index.php/form_lapor/tambahgambar'); ?>">LAPOR! HOME</a></div>
+		<div class="tab"><a href="<?php echo base_url('index.php/welcome'); ?>">LAPOR! DETAIL</a></div>
+	</div>
+</div>
+</body>
+</html>
