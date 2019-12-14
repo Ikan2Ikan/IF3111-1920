1ecb56e45c8160d1f9af62662ceaa2c9b99ff828
wahyuwiranti <wahyuwiranti@gmail.com>
2019-12-14 18:44:20 +0700

    Tambah posting pada folder views

---
+++ application/views/posting.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Detail Laporan</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/detail.css'?>">
+</head>	
