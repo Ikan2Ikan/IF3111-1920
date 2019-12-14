1c5cc99a54cb195c3cb6aafa13d9c5d4137b5b52
junioazhari <junioazhari@gmail.com>
2019-12-14 22:15:59 +0700

    menampilkan nama lampiran

--- application/views/halaman1.php
+++ application/views/halaman1.php
@@ -25,7 +25,7 @@
           foreach ($lapor as $data) { ?>
            <p><?php echo $data->isi_lapor; ?></p>
             <br>
-            <h4>Lampiran : //kodeuntukgambar
+            <h4>Lampiran : <?php echo $data->file ?>;
              <span><?php echo $data->tgl_lapor; ?>
               &emsp; <a href="">Lihat Selengkapnya</a>
             </span>
