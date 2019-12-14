150310fabd198b7a487fde242db7d01b5eec63bc
Titisprawilassari <45716749+Titisprawilassari@users.noreply.github.com>
2019-12-14 22:37:19 +0700

    update header

--- CodeIgniter/application/views/templates/header.php
+++ CodeIgniter/application/views/templates/header.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <head>
 	<title><?= $title?></title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/css/style.css"/>
 </head>
 
 <body>
 	    <nav>
-	     <a href="<?= site_url('home/index')?>">Home</a>
+	     <a href="<?= site_url('home/index')?>" class="back">Home</a>
+		 <script type='text/javascript' src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 	    </nav>
 
 	<h1>SIMPLE LAPOR!</h1>
