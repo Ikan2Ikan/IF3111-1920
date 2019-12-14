8d20d79e6aa68d30fbb68bdc7a84751ad5003fbe
wahyuwiranti <wahyuwiranti@gmail.com>
2019-12-15 06:52:00 +0700

    Tambah M_detail yang terhapus

---
+++ application/models/M_detail.php
@@ -0,0 +1,36 @@
+<?php
+<<<<<<< HEAD
+=======
+
+>>>>>>> master
+class M_detail extends CI_Model {
+	function __construct()
+	{
+		
+	}
+	
+	/* Menampilkan posting */
+	function list_posting()
+	{
+		$query = $this->db->query('select * from posting order by ID desc limit 20');
+		//lihat apakah ada data dalam tabel
+		$num = $query->num_rows();
+		if($num>0){
+			//Mengirimkan data array hasil query
+			return $query->result();
+			//Function result() hampir sama dengan function mysql_fetch_array()
+		}
+		else{
+			return 0;
+			//Kirimkan 0 jika tidak ada datanya
+		}
+	}
+<<<<<<< HEAD
+	
+	function hapus_data($id,$posting){
+	$this->db->where($id);
+	$this->db->delete($posting);
+	}
+=======
+>>>>>>> master
+}
