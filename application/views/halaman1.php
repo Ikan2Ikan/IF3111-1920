4efce33f616196dbdcbced91c2cf6bba794c446b
Abbi Kurnia <requiemzerophantasm@gmail.com>
2019-12-14 18:21:07 +0700

    menampilkandata di home

--- application/views/halaman1.php
+++ application/views/halaman1.php
@@ -19,23 +19,19 @@
       
       <h4>Laporan/Komentar Terakhir</h4>
       <hr>
-      <p></p><br>
+      <p>
+        <?php 
+          foreach ($lapor as $data) { ?>
+           <?php echo $data->isi_lapor; ?>
+            <br><br>
+            <h4>Lampiran : //kodeuntukgambar</h4>
+            <span> <?php echo $data->tgl_lapor; ?>
+            &emsp;&emsp; <a href="">Lihat Selengkapnya></a>
+            </span>
+            <hr>
+         <?php }
+        ?>
+      </p><br>
   </div>
-  <div class="konten2">
-    <h4>Lampiran: Gambar.jpg<span>(Waktu) <a href="">Lihat Selengkapnya></a></span></h4>
-    <hr>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
-  </div>
-  <div class="konten3">
-    <h4>Lampiran: Gambar.jpg<span>(Waktu) <a href="">Lihat Selengkapnya></a></span></h4>
-    <hr>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
-  </div>
-  <div class="konten4">
-    <h4>Lampiran: Gambar.jpg<span>(Waktu) <a href="">Lihat Selengkapnya></a></span></h4>
-    <hr>
-  </div>
-  </div>
-
 </body>
 </html>
