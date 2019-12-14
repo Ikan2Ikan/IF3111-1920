66d9355c6c4b1987ef4dd2d5ce53e9a936abedb8
recchan13 <93rezao@gmail.com>
2019-12-14 21:20:55 +0700

    perbaiki css home

--- application/views/tampilLaporan.php
+++ application/views/tampilLaporan.php
@@ -5,44 +5,56 @@
         <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
     </head>
     <body>
-    <h1>SIMPLE LAPOR!</h1>
-    
-    
-    <form action="<?php echo base_url('index.php/laporan/cari')?>" method="POST" enctype="multipart/form-data">
-        <input type="text" name="cari">
+        <div class = "wrap">
+            <div class = "header">
+                <br>
+                <h1> SIMPLE LAPOR!</h1>
+                <form action="<?php echo base_url('index.php/laporan/cari')?>" method="POST" enctype="multipart/form-data">
+                    <input type="text" name="cari">
 
-        <button type="submit" name="cari laporan" class="btn btn-primary">Cari</button>
+                    <button type="submit" name="cari laporan" class="btn btn-primary">Cari</button>
 
-    </form>
-    
-    <a href="<?php echo base_url('index.php/laporan/buatLaporan/') ?>"><button type="" name="create" class="btn btn-primary">Buat Laporan/Komentar</button></a>
-    <br>
-    <hr>
-    
-    <?php
-    echo "laporan atau komentar terakhir<br>";
+                </form>
+                <a href="<?php echo base_url('index.php/laporan/buatLaporan/') ?>"><button type="" name="create" class="btn btn-primary">Buat Laporan/Komentar</button></a>
+                <br>
+                <br>
+                
+            </div>
+            <div class = "konten">
+                <br>
+                laporan/komentar terakhir<br>
+                <hr>
+                <?php
+                
+                foreach($laporan as $data){
+                    echo "anonim<br>";
+                    $data->laporan=substr($data->laporan,0,30);
+                    echo $data->laporan."<br>";
 
-    foreach($laporan as $data){
-        echo "anonim<br>";
-        $data->laporan=substr($data->laporan,0,30);
-        echo $data->laporan."<br>";
+                    foreach($aspek as $dataAspek)
+                    if($data->id_aspek == $dataAspek->id){
+                        echo $dataAspek->nama."<br><br>";
+                    }
+                    ?>
 
-        foreach($aspek as $dataAspek)
-        if($data->id_aspek == $dataAspek->id){
-            echo $dataAspek->nama."<br><br>";
-        }
-        ?>
-
-        <?php $new_datetime = DateTime::createFromFormat ( "Y-m-d H:i:s", $data->updated_at );?>
-        
-        Waktu: <?php echo $new_datetime->format('d-m-Y H:i'); ?>  <a  href="<?php echo base_url();?>index.php/laporan/detail/<?php echo $data->id;?>">Lihat Selengkapnya</a>
-        <br>
-        <hr>
-    <?php
-    }
-    echo $this->pagination->create_links();
-    ?>
-
+                    <?php $new_datetime = DateTime::createFromFormat ( "Y-m-d H:i:s", $data->updated_at );?>
+                    
+                    <p>Waktu: <?php echo $new_datetime->format('d-m-Y H:i'); ?>  <a  href="<?php echo base_url();?>index.php/laporan/detail/<?php echo $data->id;?>">Lihat Selengkapnya</a> </p>
+                    
+                    <hr>
+                <?php
+                } ?>
+                <center><?php echo $this->pagination->create_links();?></center>
+                <br>
+            </div>
+            <div class = "footer">
+                &copy; 
+                <br>
+                Reza Octaviany . 14117062
+                <br>
+                Raras Franita . 14117063
+            </div>
+        </div>
     </body>
 </html>
 
