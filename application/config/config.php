26da2f098fb0496c67101667ddb2f04da201a27f
agusmichaelsianipar <agus010sianipar@gmail.com>
2019-12-15 00:01:18 +0700

    Configure base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PHP/TUBES_WEB/lapor/';
+$config['base_url'] = 'http://localhost/PABW-B/IF3111-1920/';
 
 /*
 |--------------------------------------------------------------------------
