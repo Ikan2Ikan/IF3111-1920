76d9c2e302b1575931d151616d1985a97164e4ed
TioFazri <tiofajri@yahoo.co.id>
2019-12-14 15:02:07 +0700

    model config view home

--- Source/application/controllers/Home.php
+++ Source/application/controllers/Home.php
@@ -2,17 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Home_model');
+		$this->load->helper('form');
+		
+	}
 
 	public function index()
 	{
-		$this->load->view('v_home.php');
+		$data ['judul'] = 'Lapor App';
+		$data ['home'] = $this->Home_model->getAllHome();
+		// $this->load->database();
+		$this->load->view('home/index',$data);
 	
-	}
-
-	public function lapor()
-	{
-		
-		$this->load->view('v_buat.php');
 	}
 
 }
