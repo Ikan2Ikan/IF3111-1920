00c899ed2c78ef4a0a5ed8a4ea33a3dbb574a90a
dannyambarita <48155543+dannyambarita@users.noreply.github.com>
2019-12-14 23:08:56 +0700

    update css

--- application/views/konten/template.php
+++ application/views/konten/template.php
@@ -6,10 +6,10 @@
            -->
             <!-- <button type class="searchbutton">  <i class="fa fa-search"> Cari</i> -->
             <form action="<?php echo base_url().'index.php/search'?>" method="GET">
-                <input type="text" name ="keyword" placeholder="Cari...">
-                <input type="submit"value="Cari"><br>
+                <input type="text" name ="keyword" id="key" placeholder="Cari...">
+                <input type="submit" id="sub" value="Cari"><br>
             </form>
-       
+        
         <div class="buat">
         <a href="<?php echo base_url().'index.php/buatlaporan'; ?>">
             Buat Laporan/Komentar <i class="fa fa-plus-square"></i>
@@ -24,7 +24,7 @@
                 <?php }else {?>          
                         <p class="isi"><?php echo substr ($item_laporan["isi_laporan"],0,300); ?>...</p>
                 <?php }?>
-                <div class="waktu">waktu: <?php echo $item_laporan["waktu_laporan"];?>  
+                <div class="waktu">Waktu: <?php echo $item_laporan["waktu_laporan"];?>  
                 
                
                 <div class="detail">
