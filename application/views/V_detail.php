e6286a9a2ee1382b171107cc01aeda0b0c0e924a
audinamia <miaaudina1223@gmail.com>
2019-12-14 09:16:38 +0700

    menambahkan tampilan detail

--- application/views/V_detail.php
+++ application/views/V_detail.php
@@ -2,25 +2,24 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>LAPOR! ITERA</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="<?= base_url('assets/style_detail.css') ?>">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <title><?= $judul; ?></title>
 </head>
 
 <body>
+<body>
     <div class="container">
         <div>
-            <h1 class="judul">LAPOR! ITERA</h1>
+            <h1 class="judul">ITERA LAPOR!</h1>
         </div>
-
         <div>
             <p class="laporan">Detail Laporan/Komentar</p>
         </div>
-
-        <hr>
-        </hr>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi at tempora eius dolorem assumenda libero, laboriosam deserunt animi illo earum asperiores quos nisi repellendus rerum vel maiores eum cupiditate enim?</p>
 
     </div>
