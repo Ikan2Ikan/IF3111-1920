5c57b486aab60de361eae48ebbe240c9136471ec
wahyuwiranti <wahyuwiranti@gmail.com>
2019-12-14 18:42:13 +0700

    Tambah Detail pada folder controllers

---
+++ application/controllers/Detail.php
@@ -0,0 +1,15 @@
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Detail extends CI_Controller {
+	public function index()
+	{
+		echo 'Selamat datang di CodeIgniter';
+	}
+	
+	function posting()
+	{
+		$this->load->model('M_detail');
+		$data['posting'] = $this->M_detail->list_posting();
+		$this->load->view('posting',$data);
+	}
+}
